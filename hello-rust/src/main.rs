dd4e22654b8ed6a305ea3154fcef350227635c79
ZhengjunHUO <firelouiszj@hotmail.com>
2022-11-05 16:15:38 +0100

    first step
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

---
+++ hello-rust/src/main.rs
@@ -0,0 +1,13 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
+fn main() {
+    println!("Hello World!");
+
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
+}
