aad5174e3287859a1dd4f5ceec43aa13b652021b
ZhengjunHUO <firelouiszj@hotmail.com>
2022-11-06 23:09:19 +0100

    explore func and loop
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

---
+++ functions/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let mut max = 0;
+    let list: [u32; 5] = [12, 7, 88, 36, 1]; 
+
+    for v in list {
+        max = max_positive(max, v);
+    }
+
+    println!("Result: Array's max is {max}. Quiting...");
+
+    for countdown_num in (1..4).rev() {
+        println!("{countdown_num}");
+    }
+
+    println!("Bye.");
+}
+
+fn max_positive(x: u32, y: u32) -> u32 {
+    if x > y {
+        return x
+    }
+
+    y
+}
