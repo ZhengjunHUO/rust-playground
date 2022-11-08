38bbc8553a82fde02ef0c8ac277c952dc3e67d18
ZhengjunHUO <firelouiszj@hotmail.com>
2022-11-08 09:48:54 +0100

    update ownership
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -19,14 +19,46 @@ fn main() {
 
     // s1 moved into func, not valid any more
     let (s3, len) = get_len(s1);
-    println!("{}'s length is {}", s3, len)
+    println!("{}'s length is {}", s3, len);
 
     // also works
     //let (s1, len) = get_len(s1);
     //println!("{}'s length is {}", s1, len)
+
+    // use immutable ref, avoid move back the origin string from the func
+    let lenth = calc_len(&s3);
+    println!("{}'s lengthw is {}", s3, lenth);
+
+    // modify mutable ref in func
+    let mut s4 = String::from("你好");
+
+    // can have several immut refs
+    let r3 = &s4;
+    let r4 = &s4;
+    println!("{} {}", r3, r4);  // r3, r4 scope end here, ok to have mut ref
+
+    // not the case for mut refs (avoid race condition)
+    let r1 = &mut s4;
+    modif_str_ch(r1);  // r1's scope end here, ok to have another mutable ref
+
+    let r2 = &mut s4;
+    modif_str_en(r2);
+    println!("After change: {}", s4);
 }
 
 fn get_len(s: String) -> (String, usize) {
     let len = s.len();
     (s, len)
+}
+
+fn calc_len(s: &String) -> usize {
+    s.len()
+}
+
+fn modif_str_ch(s: &mut String) {
+    s.push_str(", 世界!");
+}
+
+fn modif_str_en(s: &mut String) {
+    s.push_str(" world!");
 }
