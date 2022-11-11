e0c2fcef4bc23692597987a1632949e5f197fcd0
ZhengjunHUO <firelouiszj@hotmail.com>
2022-11-11 18:07:09 +0100

    if let syntax
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- enum_match/src/main.rs
+++ enum_match/src/main.rs
@@ -7,11 +7,21 @@ fn add_i32(num: Option<i32>, delta: i32) -> Option<i32> {
     }
 }
 
+fn get_value_i32(num: Option<i32>) -> i32 {
+    // if let syntax
+    if let Some(value) = num {
+        //println!("The value is {}", value);
+        value
+    } else {
+        0
+    }
+}
+
 fn main() {
     // test option enum
     let num_orig = Some(3);
     let num = add_i32(num_orig, FIVE);
-    println!("{:?} adds {} equals to {:?}", num_orig, FIVE, num);
+    println!("{} adds {} equals to {}", get_value_i32(num_orig), FIVE, get_value_i32(num));
 
     let none_orig = None;
     let none = add_i32(none_orig, FIVE);
