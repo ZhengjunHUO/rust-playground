618bb3e8f834d08a6b97894e7ea99f1e4eece885
ZhengjunHUO <firelouiszj@hotmail.com>
2022-11-11 17:29:20 +0100

    add match to enum
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- enum_test/src/main.rs
+++ enum_test/src/main.rs
@@ -6,25 +6,27 @@ enum IpAddr {
 }
 
 #[derive(Debug)]
+#[allow(unused)]
 struct IpAddrV4 {
-    addr: (u8, u8, u8, u8),
+    addr: [u8; 4],
     cidr: u8,
 }
 
 #[derive(Debug)]
+#[allow(unused)]
 struct IpAddrV6 {
-    addr: (u16, u16, u16, u16, u16, u16, u16, u16),
+    addr: [u16; 8],
     cidr: u8,
 }
 
 fn main() {
     let ip4 = IpAddr::V4(IpAddrV4{
-        addr: (127, 0, 0, 1),
+        addr: [127, 0, 0, 1],
         cidr: 8,
     });
 
     let ip6 = IpAddr::V6(IpAddrV6{
-        addr: (0, 0, 0, 0, 0, 0, 0, 1),
+        addr: [0, 0, 0, 0, 0, 0, 0, 1],
         cidr: 128,
     });
 
@@ -33,5 +35,25 @@ fn main() {
 }
 
 fn trace_route(ip: &IpAddr) {
-    println!("Tracing the route to {:?} ...", ip);
+    match ip {
+        IpAddr::V4(ipv4) => {
+            let mut addr = String::from(" ");
+            let dot = String::from(".");
+            let slash = String::from("/");
+            let len = ipv4.addr.len();
+            for (i, &v) in ipv4.addr.iter().enumerate() {
+                addr.push_str(&v.to_string());
+		if i < len - 1 {
+                    addr.push_str(&dot);
+                }else{
+                    addr.push_str(&slash);
+                }
+            }
+            addr.push_str(&ipv4.cidr.to_string());
+            println!("Tracing the route to an IPv4 addr: {:?} ...", addr);
+        }
+        IpAddr::V6(ipv6) =>
+            println!("Tracing the route to an IPv6 addr: {:?}/{:?} ...", ipv6.addr, ipv6.cidr),
+        // _ => (),
+    }
 }
