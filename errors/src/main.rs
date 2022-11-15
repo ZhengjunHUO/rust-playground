86d20c80e94dad104380aabcb7432a7dac3ec2d8
ZhengjunHUO <firelouiszj@hotmail.com>
2022-11-15 11:04:50 +0100

    update error handling logic
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,11 +1,8 @@
-use std::fs::File;
-use std::fs::OpenOptions;
-use std::io::ErrorKind;
-use std::io::Write;
-use std::io::Read;
+use std::fs::{File,OpenOptions};
+use std::io::{ErrorKind,Write,Read,Seek};
 
 fn main() {
-    // name typed string slice
+    // file name typed string slice
     let filename = "foo.txt";
 
     // try to open a file (read-only mode)
@@ -13,29 +10,38 @@ fn main() {
     let mut f = match rslt {
         // file exist
         Ok(file) => file,
-        // other open error
+        // error while opening
         Err(error) => match error.kind() {
-            // if file not exist, create it (write-only mode)
+            // if file not exist, create it (File::create: write-only mode, need to use OpenOptions here)
             ErrorKind::NotFound => match OpenOptions::new()
                 .read(true)
                 .write(true)
                 .create(true)
                 .open(filename) {
                      Ok(mut created_file) => {
+                         // Write to the created file
                          match created_file.write_all(b"Hello Rust!") {
-                             Ok(_) => created_file,
+                             // Rewind cursor to the begining of the file
+                             Ok(_) => match created_file.rewind() {
+                                 Ok(_) => {
+                                     println!("{} not exist, create it", filename);
+                                     created_file
+                                 }
+                                 Err(err) => panic!("Failed to rewind: {:?}", err),
+                             }
                              Err(err) => panic!("Failed to write to created file: {:?}", err),
                          }
                      },
                      Err(err) => panic!("Failed to create: {:?}", err),
             },
-            // other unknown errors
+            // all other unknown errors
             _ => panic!("Failed to open: {:?}", error),
         },
     };
 
     println!("file's info: {:?}", f);
     let mut buf = String::new();
+    // read the file's content
     match f.read_to_string(&mut buf) {
         Ok(_) => println!("file's content: {:?}", buf),
         Err(err) => panic!("Failed to dump file: {:?}", err),
