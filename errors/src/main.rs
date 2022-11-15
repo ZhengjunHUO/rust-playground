831ae1f0a93892839f8fec0f07a15405ab147850
ZhengjunHUO <firelouiszj@hotmail.com>
2022-11-15 13:25:01 +0100

    use closure and expect
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -6,14 +6,13 @@ fn main() {
     let filename = "foo.txt";
 
     // try to open a file (read-only mode)
-    let rslt = File::open(filename);
-    let mut f = match rslt {
-        // file exist
-        Ok(file) => file,
+    // (1) use closure and the unwrap_or_else method here
+    let mut f = File::open(filename).unwrap_or_else(|error| {
         // error while opening
-        Err(error) => match error.kind() {
+        if error.kind() == ErrorKind::NotFound {
             // if file not exist, create it (File::create: write-only mode, need to use OpenOptions here)
-            ErrorKind::NotFound => match OpenOptions::new()
+            // (2) use match
+            match OpenOptions::new()
                 .read(true)
                 .write(true)
                 .create(true)
@@ -33,17 +32,22 @@ fn main() {
                          }
                      },
                      Err(err) => panic!("Failed to create: {:?}", err),
-            },
+            }
+        }else{
             // all other unknown errors
-            _ => panic!("Failed to open: {:?}", error),
-        },
-    };
+            panic!("Failed to open: {:?}", error);
+        }
+    });
 
     println!("file's info: {:?}", f);
     let mut buf = String::new();
+
     // read the file's content
-    match f.read_to_string(&mut buf) {
-        Ok(_) => println!("file's content: {:?}", buf),
-        Err(err) => panic!("Failed to dump file: {:?}", err),
-    }
+    //match f.read_to_string(&mut buf) {
+    //    Ok(_) => println!("file's content: {:?}", buf),
+    //    Err(err) => panic!(
+    //}
+    // (3) use expect
+    f.read_to_string(&mut buf).expect("Failed to dump file");
+    println!("file's content: {:?}", buf);
 }
