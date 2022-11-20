0adc03c1facaafd773de6f26e190e04eb03dc0f9
ZhengjunHUO <firelouiszj@hotmail.com>
2022-11-20 13:28:35 +0100

    mix generic,trait,lifecycle
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- lifetime/src/main.rs
+++ lifetime/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 fn main() {
     let s1 = String::from("Hello, Rust!");
     let rslt;
@@ -15,10 +17,25 @@ fn main() {
 
     let rslt2 = new_str();
     println!("result2 is: {}", rslt2);
+
+    let rslt3 = max_str_2(s1.as_str(), "foo", "result3 is");
+    println!("{}", rslt3);
 }
 
 // the lifetime of the reference in result must be the smaller lifetime of the two arguments
 fn max_str<'a>(s1: &'a str, s2: &'a str) -> &'a str {
+    if s1.len() > s2.len() {
+        s1
+    } else {
+        s2
+    }
+}
+
+fn max_str_2<'a, T>(s1: &'a str, s2: &'a str, obj: T) -> &'a str
+where
+    T: Display,
+{
+    println!("{}:", obj);
     if s1.len() > s2.len() {
         s1
     } else {
