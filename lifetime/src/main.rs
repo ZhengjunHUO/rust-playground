a12768d97dfc9bbaea5a4ff9e1a939bfeb7b44e9
ZhengjunHUO <firelouiszj@hotmail.com>
2022-11-20 13:20:49 +0100

    learn lifetime
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

---
+++ lifetime/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let s1 = String::from("Hello, Rust!");
+    let rslt;
+
+    {
+        let s2 = "huo";
+        rslt = max_str(s1.as_str(), s2);
+
+        // won't work, rslt's lifetime == s2's lifetime, end at inner }
+        //let s2 = String::from("huo");
+        //rslt = max_str(s1.as_str(), s2.as_str());
+    }
+
+    println!("result is: {}", rslt);
+
+    let rslt2 = new_str();
+    println!("result2 is: {}", rslt2);
+}
+
+// the lifetime of the reference in result must be the smaller lifetime of the two arguments
+fn max_str<'a>(s1: &'a str, s2: &'a str) -> &'a str {
+    if s1.len() > s2.len() {
+        s1
+    } else {
+        s2
+    }
+}
+
+fn new_str() -> &'static str {
+    let rslt = "fufu";
+    return rslt
+}
