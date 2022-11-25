23f36eab24e27d9f3636d4c257674be13d47fdd9
ZhengjunHUO <firelouiszj@hotmail.com>
2022-11-25 21:47:01 +0100

    closure capture value
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,3 +1,5 @@
+use std::thread;
+
 fn increment_func (x: u32) -> u32 { x + 1 }
 
 fn main() {
@@ -13,4 +15,50 @@ fn main() {
     x = increment_closure2(x);
     x = increment_closure3(x);
     println!("Apply 3 closures, get {}", x);
+
+    println!("=== Test immutable borrow ===");
+    borrow();
+    println!("=== Test mutable borrow ===");
+    mut_borrow();
+    println!("=== Test move ownership ===");
+    move_ownership();
+}
+
+fn borrow() {
+    let prime = vec![2, 3, 5];
+    println!("Before defining a closure: {:?}", prime);
+
+    // immutable borrow => immutable binding
+    let borrow_closure = || println!("[Inside closure] prime list: {:?}", prime);
+
+    // immutable borrow occurs, multiple immu ref can coexist, will compile
+    println!("[Before closure] prime list: {:?}", prime);
+    borrow_closure();
+    println!("[After  closure] prime list: {:?}", prime);
+}
+
+fn mut_borrow() {
+    let mut prime = vec![2, 3, 5];
+    println!("Before defining a closure: {:?}", prime);
+
+    // mutable borrow => mutable binding
+    let mut borrow_mut_closure = || prime.push(7);
+
+    // immutable borrow occurs when there is already a mutable borrow, won't compile
+    // println!("[Before closure] prime list: {:?}", prime);
+
+    borrow_mut_closure();
+    // mutable borrow end here, can print
+    println!("[After  closure] prime list: {:?}", prime);
+}
+
+fn move_ownership() {
+    let prime = vec![2, 3, 5];
+    println!("Define a prime list: {:?}", prime);
+
+    // value moved into closure, closure used as thread's arg
+    thread::spawn(move || println!("[In thread] prime list: {:?}", prime)).join().unwrap();
+
+    // value moved, won't compile
+    //println!("[After closure] prime list: {:?}", prime);
 }
