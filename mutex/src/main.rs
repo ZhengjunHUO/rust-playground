004dad1e28ebda193d189750ce52b10d0db45cf9
ZhengjunHUO <firelouiszj@hotmail.com>
2022-12-01 22:50:51 +0100

    mutex with multithreads
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

---
+++ mutex/src/main.rs
@@ -0,0 +1,24 @@
+use std::thread;
+use std::sync::{Mutex,Arc};
+
+fn main() {
+    let count = 5;
+    let mx = Arc::new(Mutex::new(0));
+    let mut hs = vec![];
+
+    for _ in 0..count {
+        let c = Arc::clone(&mx);
+        let h = thread::spawn(move || {
+            let mut n = c.lock().unwrap();
+            *n += 1
+        });
+        hs.push(h);
+    }
+
+    for h in hs {
+        h.join().unwrap();
+    }
+
+    assert_eq!(count, *mx.lock().unwrap());
+    println!("The mutex is {:?}", mx);
+}
