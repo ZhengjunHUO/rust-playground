691254c0fb89f7f1565e15d134f8e3fc9a860f1c
ZhengjunHUO <firelouiszj@hotmail.com>
2022-12-03 22:36:20 +0100

    pattern destructuring
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -48,4 +48,75 @@ fn main() {
     }
 
     println!("x: {:?}, outer y: {}", x, y);
+
+    // (4) Multi Patterns
+    //
+    let num = 18;
+    match num {
+        1 | 2 | 3 => println!("num is 1/2/3"),
+        4..=20 => println!("num is between 4-20"),
+        _ => println!("something else"),
+    }
+
+    let c = 'q';
+    match c {
+        'a'..='m' => println!("belong to the first half of lowercase alphabet"),
+        'n'..='z' => println!("belong to the second half of lowercase alphabet"),
+        _ => println!("some non lowercase alphabet char"),
+    }
+
+    // (5) pattern used for destructuring
+    //
+    let c = Cordinate{ x: 9, y: 0 };
+    destructuring_struct(&c);
+
+    let s = Sketchbook::SetColor(ColorMode::Rgb(125, 100, 250));
+    destructuring_enum(s);
+
+    destructuring_enum(Sketchbook::SetColor(ColorMode::Hsv(25, 10, 200)));
+    destructuring_enum(Sketchbook::CursorTo{ x: 20, y: 30 });
+    destructuring_enum(Sketchbook::Draw(String::from("Hello Rust!")));
+    destructuring_enum(Sketchbook::Close);
+}
+
+struct Cordinate {
+    x: i32,
+    y: i32,
+}
+
+fn destructuring_struct(c: &Cordinate) {
+    match c {
+        // match c.x == 0
+        Cordinate { x: 0, y } => println!("c is on the y axis, offset {}", y),
+        // match c.y == 0
+        Cordinate { x, y: 0 } => println!("c is on the x axis, offset {}", x),
+        Cordinate { x, y } => println!("c({}, {}) is not on the axis", x, y),
+    }
+
+    // break apart c, store the value in x, y
+    let Cordinate { x, y } = c;
+    assert_eq!(*x, 9);
+    assert_eq!(*y, 0);
+}
+
+enum ColorMode {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
+}
+
+enum Sketchbook {
+    CursorTo { x: i32, y: i32}, // struct-like enum variant
+    SetColor(ColorMode), // nested enum
+    Draw(String), // tuple-like enum variant (single element)
+    Close, // enum variant without any data
+}
+
+fn destructuring_enum(s: Sketchbook) {
+    match s {
+        Sketchbook::CursorTo { x, y } => println!("Cursor moved to ({}, {})", x, y),
+        Sketchbook::SetColor(ColorMode::Rgb(r, g, b)) => println!("Set brush color to RGB({}, {}, {})", r, g, b),
+        Sketchbook::SetColor(ColorMode::Hsv(h, s, v)) => println!("Set brush color to HSV({}, {}, {})", h, s, v),
+        Sketchbook::Draw(s) => println!("Drawing {s}"),
+        Sketchbook::Close => println!("Quit."),
+    }
 }
