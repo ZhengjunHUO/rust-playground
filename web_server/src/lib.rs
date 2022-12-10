e0fd20d815da320641eed6db35017a199e31f487
ZhengjunHUO <firelouiszj@hotmail.com>
2022-12-10 21:01:41 +0100

    impl workerpool
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- web_server/src/lib.rs
+++ web_server/src/lib.rs
@@ -1,8 +1,11 @@
-use std::thread;
+use std::{
+    thread,
+    sync::{mpsc, Arc, Mutex},
+};
 
 pub struct WorkerPool {
-//    workers: Vec<
     workers: Vec<Worker>,
+    tx: mpsc::Sender<Task>,
 }
 
 struct Worker {
@@ -19,26 +22,37 @@ impl WorkerPool {
     pub fn new(s: usize) -> WorkerPool {
         assert!(s > 0);
 
+        let (tx, rx) = mpsc::channel();
+        let rx = Arc::new(Mutex::new(rx));
+
         let mut workers = Vec::with_capacity(s);
 
         for id in 0..s {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&rx)));
         }
 
-        WorkerPool { workers }
+        WorkerPool { workers, tx }
     }
 
     pub fn schedule<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+        let task = Box::new(f);
+        self.tx.send(task).unwrap();
     }
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, rx: Arc<Mutex<mpsc::Receiver<Task>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let task = rx.lock().unwrap().recv().unwrap();
+            println!("Task scheduled to worker {id}");
+            task();
+        });
 
 	Worker { id, thread }
     }
 }
+
+type Task = Box<dyn FnOnce() + Send + 'static>;
--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -24,6 +24,7 @@ fn main() {
         wp.schedule(|| {
 	    handle_conn(conn);
         });
+        println!("[DEBUG] Conn dispatched!");
     }
 }
 
