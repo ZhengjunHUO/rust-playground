38829a843d0f5e454a7ca1838e46fd2bd6346b85
ZhengjunHUO <firelouiszj@hotmail.com>
2022-12-10 12:23:15 +0100

    add req check logic
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -2,6 +2,10 @@ use std::net::{TcpListener, TcpStream};
 use std::io::{prelude::*, BufReader};
 use std::fs;
 
+const RESP_OK_STATUS: &str = "HTTP/1.1 200 OK";
+const RESP_NOT_FOUND_STATUS: &str = "HTTP/1.1 404 NOT FOUND";
+const REQ_FORMAT: &str = "GET / HTTP/1.1";
+
 fn main() {
     let l = TcpListener::bind("127.0.0.1:8080").unwrap();
 
@@ -16,15 +20,22 @@ fn main() {
 
 fn handle_conn(mut conn: TcpStream) {
     let rdr = BufReader::new(&mut conn);
+
     // read the http request until a new empty line ("\n\n")
-    let req: Vec<_> = rdr.lines().map(|rslt| rslt.unwrap()).take_while(|line| !line.is_empty()).collect();
+    //let req: Vec<_> = rdr.lines().map(|rslt| rslt.unwrap()).take_while(|line| !line.is_empty()).collect();
 
-    println!("[DEBUG] Recv conn req: {:#?}", req);
+    let req_format = rdr.lines().next().unwrap().unwrap();
+    println!("[DEBUG] Recv conn req: {:#?}", req_format);
 
-    let status = "HTTP/1.1 200 OK";
-    let payload = fs::read_to_string("index.html").unwrap();
+    let (filename, resp_status) = if req_format == REQ_FORMAT {
+        ("index.html", RESP_OK_STATUS)
+    } else {
+        ("404.html", RESP_NOT_FOUND_STATUS)
+    };
+
+    let payload = fs::read_to_string(filename).unwrap();
     let len = payload.len();
+    let resp = format!("{resp_status}\r\nContent-Length: {len}\r\n\r\n{payload}");
 
-    let resp = format!("{status}\r\nContent-Length: {len}\r\n\r\n{payload}");
     conn.write_all(resp.as_bytes()).unwrap();
 }
