b232b27c0e14e0d5de92356c0214927cb0bfab70
ZhengjunHUO <firelouiszj@hotmail.com>
2022-12-18 09:45:29 +0100

    derive copy test
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- copy_clone/src/main.rs
+++ copy_clone/src/main.rs
@@ -5,18 +5,22 @@
 struct DeriveCopyClone {
     a: u32,
     b: u32,
+    //s并没有implement Copy所以本struct不能derive Copy
+    //s: String,
 }
 
 #[derive(Debug, Clone)]
 struct DeriveClone {
     a: u32,
     b: u32,
+    s: String,
 }
 
 #[derive(Debug)]
 struct Couple {
     a: u32,
     b: u32,
+    s: String,
 }
 
 fn main() {
@@ -26,13 +30,18 @@ fn main() {
     let dcc2 = dcc1;
     println!("dcc1: {:?}, dcc2: {:?}", dcc1, dcc2);
 
-    let dc1 = DeriveClone { a: 3, b: 4 };
+    let dc1 = DeriveClone { a: 3, b: 4, s: String::from("Rust rocks!"), };
     // 只derive了Clone，所以需要显示地复制到dc2
     let dc2 = dc1.clone();
     println!("dc1: {:?}, dc2: {:?}", dc1, dc2);
 
-    let c1 = Couple { a: 7, b: 8 };
+    let c1 = Couple { a: 7, b: 8, s: String::from("Rust rocks!"), };
     // 没有derive，所以是move
     let c2 = c1;
     println!("c2: {:?}", c2);
+
+
+    let s1 = String::from("Rust rocks!");
+    let s2 = s1.clone();
+    println!("{s1} {s2}");
 }
