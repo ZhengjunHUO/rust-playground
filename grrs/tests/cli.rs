2265f2b8ad777eb36161265f253eddf20f92d43a
ZhengjunHUO <firelouiszj@hotmail.com>
2022-12-24 12:10:19 +0100

    add integration test with success case
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- grrs/tests/cli.rs
+++ grrs/tests/cli.rs
@@ -1,4 +1,5 @@
 use assert_cmd::prelude::*;
+use assert_fs::prelude::*;
 use predicates::prelude::*;
 use std::process::Command;
 
@@ -9,6 +10,20 @@ fn file_not_found() -> Result<(), Box<dyn std::error::Error>> {
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("Failed to open file"));
+
+    Ok(())
+}
+
+#[test]
+fn file_exist_and_match() -> Result<(), Box<dyn std::error::Error>> {
+    let f = assert_fs::NamedTempFile::new("temp.txt")?;
+    f.write_str("A foo file\nSome bar content\nSome baz content\nSome foo content.")?;
+
+    let mut cmd = Command::cargo_bin("grrs")?;
+    cmd.arg("-t").arg("foo").arg("-p").arg(f.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("A foo file\nSome foo content."));
 
     Ok(())
 }
