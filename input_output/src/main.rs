b53b1c2846489ffec3a14705da7e0896edba4a1b
ZhengjunHUO <firelouiszj@hotmail.com>
2022-12-25 10:34:54 +0100

    accept mandatory <PATH_TO_FILE> argument
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- input_output/src/main.rs
+++ input_output/src/main.rs
@@ -1,23 +1,37 @@
 use clap::Parser;
 use serde_json::json;
+use std::{fs, path::PathBuf};
 
 #[derive(Parser)]
+#[command(arg_required_else_help = true)]
 struct Params {
+    file: PathBuf,
+
+    /// Output in json format
     #[arg(short = 'j', long = "json")]
     json: bool,
 }
 
 fn main() {
     let args = Params::parse();
+    let mut c = 0;
+    let f = args.file;
+
+    for l in fs::read_to_string(&f).unwrap().lines() {
+        c += l.split(' ').count();
+    }
+
+    let rslt = format!("`{}`'s word count: {}", f.to_str().unwrap(), c);
+
     if args.json {
         println!(
             "{}",
             json!({
                 "type": "info",
-                "message": "This is rust speaking",
+                "message": rslt,
             })
         );
     } else {
-        println!("This is rust speaking");
+        println!("{}", rslt);
     }
 }
