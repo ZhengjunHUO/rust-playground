56bedb0c6394d393fc2302aa0626db30877d335f
ZhengjunHUO <firelouiszj@hotmail.com>
2022-12-25 11:10:03 +0100

    accept input from stdin
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

---
+++ input_output/README.md
@@ -0,0 +1,6 @@
+```sh
+# Read input from stdin
+echo "Hello from Rust" | cargo run -q -- -
+# Read input from file
+cargo run -q -- /etc/hosts
+```
--- input_output/src/main.rs
+++ input_output/src/main.rs
@@ -1,6 +1,12 @@
-use clap::Parser;
+use clap::{CommandFactory, Parser};
+use is_terminal::IsTerminal;
 use serde_json::json;
-use std::{fs, path::PathBuf};
+use std::{
+    fs::File,
+    io::{stdin, BufRead, BufReader},
+    path::PathBuf,
+    process::exit,
+};
 
 #[derive(Parser)]
 #[command(arg_required_else_help = true)]
@@ -12,16 +18,39 @@ struct Params {
     json: bool,
 }
 
+fn count_words<R: BufRead>(r: R) -> usize {
+    let mut c = 0;
+
+    for l in r.lines() {
+        c += l.unwrap().split(' ').count();
+    }
+    c
+
+    // when f is PathBuf
+    //for l in std::fs::read_to_string(&f).unwrap().lines() {
+    //    c += l.split(' ').count();
+    //}
+}
+
 fn main() {
     let args = Params::parse();
-    let mut c = 0;
-    let f = args.file;
 
-    for l in fs::read_to_string(&f).unwrap().lines() {
-        c += l.split(' ').count();
+    let mut f = args.file;
+    let c;
+
+    if f == PathBuf::from("-") {
+        if stdin().is_terminal() {
+            Params::command().print_help().unwrap();
+            exit(1);
+        }
+
+        f = PathBuf::from("<stdin>");
+        c = count_words(BufReader::new(stdin().lock()));
+    } else {
+        c = count_words(BufReader::new(File::open(&f).unwrap()));
     }
 
-    let rslt = format!("`{}`'s word count: {}", f.to_str().unwrap(), c);
+    let rslt = format!("`{}`'s word count: {}", f.to_string_lossy(), c);
 
     if args.json {
         println!(
