39b2b675aaf2364964ab7b76abf6b6454911f54c
ZhengjunHUO <firelouiszj@hotmail.com>
2022-12-26 17:09:50 +0100

    add plotting components
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- mandelbrot/src/lib.rs
+++ mandelbrot/src/lib.rs
@@ -1,5 +1,6 @@
+use image::{png::PNGEncoder, ColorType};
 use num::Complex;
-use std::str::FromStr;
+use std::{fs::File, io::Error, str::FromStr};
 
 /// Parse a string slice `s` containing two values, seperated by a delimiter `d` to a tuple
 fn get_pair<T: FromStr>(s: &str, d: char) -> Option<(T, T)> {
@@ -37,24 +38,60 @@ fn is_mandelbrot_set_member(c: Complex<f64>, max_iter: usize) -> Option<usize> {
 }
 
 /// Map a pixel's coordiantes to a Complex
-/// `panel_in_pixel`: size of canvas in pixel
-/// `panel_in_complex`: the size is designated by
+/// `pixel_range`: size of canvas in pixel
+/// `complex_range`: the size is designated by
 ///    a upper left complex and a lower_right complex
 /// `pixel`: the pixel to be converted
-fn pixel2point(
-    panel_in_pixel: (usize, usize),
-    panel_in_complex: (Complex<f64>, Complex<f64>),
+fn pixel2complex(
+    pixel_range: (usize, usize),
+    complex_range: (Complex<f64>, Complex<f64>),
     pixel: (usize, usize),
 ) -> Complex<f64> {
     let (w, h) = (
-        panel_in_complex.1.re - panel_in_complex.0.re,
-        panel_in_complex.0.im - panel_in_complex.1.im,
+        complex_range.1.re - complex_range.0.re,
+        complex_range.0.im - complex_range.1.im,
     );
 
     Complex {
-        re: panel_in_complex.0.re + pixel.0 as f64 * w / panel_in_pixel.0 as f64,
-        im: panel_in_complex.0.im - pixel.1 as f64 * h / panel_in_pixel.1 as f64,
+        re: complex_range.0.re + pixel.0 as f64 * w / pixel_range.0 as f64,
+        im: complex_range.0.im - pixel.1 as f64 * h / pixel_range.1 as f64,
     }
+}
+
+/// Map a rectangle range of pixels to Complex
+/// then test if the Complex is a member of set
+/// and parse the result to a grayscale value
+/// eg. is a member => return None => 0 => black
+fn render(
+    pixel_range: (usize, usize),
+    complex_range: (Complex<f64>, Complex<f64>),
+    pixels: &mut [u8],
+) {
+    assert!(pixels.len() == pixel_range.0 * pixel_range.1);
+
+    for row in 0..pixel_range.1 {
+        for col in 0..pixel_range.0 {
+            let cp = pixel2complex(pixel_range, complex_range, (col, row));
+
+            pixels[row * pixel_range.0 + col] = match is_mandelbrot_set_member(cp, 255) {
+                None => 0,
+                Some(iter) => 255 - iter as u8,
+            };
+        }
+    }
+}
+
+/// Write pixels (with grayscale value) to a png image
+fn save_to_img(name: &str, pixel_range: (usize, usize), pixels: &[u8]) -> Result<(), Error> {
+    let f = File::create(name)?;
+    let enc = PNGEncoder::new(f);
+    enc.encode(
+        pixels,
+        pixel_range.0 as u32,
+        pixel_range.1 as u32,
+        ColorType::Gray(8),
+    )?;
+    Ok(())
 }
 
 #[cfg(test)]
@@ -82,9 +119,9 @@ mod tests {
     }
 
     #[test]
-    fn test_pixel2point() {
+    fn test_pixel2complex() {
         assert_eq!(
-            pixel2point(
+            pixel2complex(
                 (100, 200),
                 (Complex { re: -1.0, im: 1.0 }, Complex { re: 1.0, im: -1.0 }),
                 (25, 175)
