e4240cc24e47cc9173daefc76f804a4c4fd6f5e3
ZhengjunHUO <firelouiszj@hotmail.com>
2023-01-10 14:42:36 +0100

    solve leetcode 0787
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

---
+++ leetcode/src/graph.rs
@@ -0,0 +1,68 @@
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashMap};
+
+#[derive(Debug)]
+struct Flight {
+    node_id: i32,
+    hops_left: i32,
+    cost_accumulated: i32,
+}
+
+impl PartialEq for Flight {
+    fn eq(&self, other: &Self) -> bool {
+        self.cost_accumulated == other.cost_accumulated
+    }
+}
+
+impl Eq for Flight {}
+
+impl Ord for Flight {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.cost_accumulated.cmp(&other.cost_accumulated).reverse()
+    }
+}
+
+impl PartialOrd for Flight {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+pub fn find_cheapest_price(n: i32, flights: Vec<Vec<i32>>, src: i32, dst: i32, k: i32) -> i32 {
+    let mut dict = HashMap::<i32, Vec<[i32; 2]>>::new();
+
+    for f in flights {
+        dict.entry(f[0])
+            .and_modify(|e| e.push([f[1], f[2]]))
+            .or_insert(vec![[f[1], f[2]]]);
+    }
+
+    let mut pq = BinaryHeap::new();
+    pq.push(Flight {
+        node_id: src,
+        hops_left: k + 1,
+        cost_accumulated: 0,
+    });
+
+    while pq.len() > 0 {
+        let curr = pq.pop().unwrap();
+        if curr.node_id == dst {
+            return curr.cost_accumulated;
+        }
+
+        if curr.hops_left > 0 {
+            if dict.contains_key(&curr.node_id) {
+                let neigh = dict.get(&curr.node_id).unwrap();
+                for i in neigh {
+                    pq.push(Flight {
+                        node_id: i[0],
+                        hops_left: curr.hops_left - 1,
+                        cost_accumulated: curr.cost_accumulated + i[1],
+                    });
+                }
+            }
+        }
+    }
+
+    return -1;
+}
--- leetcode/src/lib.rs
+++ leetcode/src/lib.rs
@@ -1,5 +1,7 @@
+mod graph;
 mod hashtable;
 
+use crate::graph::*;
 use crate::hashtable::*;
 
 #[cfg(test)]
@@ -8,7 +10,11 @@ mod tests {
 
     #[test]
     fn test_two_sum() {
-        let mut nums = [Some(vec![2, 7, 11, 15]), Some(vec![3, 2, 4]), Some(vec!(3, 3))];
+        let mut nums = [
+            Some(vec![2, 7, 11, 15]),
+            Some(vec![3, 2, 4]),
+            Some(vec![3, 3]),
+        ];
         let target = [9, 6, 6];
         let result = [vec![0, 1], vec![1, 2], vec![0, 1]];
 
@@ -16,5 +22,14 @@ mod tests {
         for i in 0..size {
             assert_eq!(two_sum(nums[i].take().unwrap(), target[i]), result[i]);
         }
+    }
+
+    #[test]
+    fn test_find_cheapest_price() {
+        let mut f = vec![vec![0, 1, 100], vec![1, 2, 100], vec![0, 2, 500]];
+        assert_eq!(find_cheapest_price(3, f, 0, 2, 1), 200);
+
+        f = vec![vec![0, 1, 100], vec![1, 2, 100], vec![0, 2, 500]];
+        assert_eq!(find_cheapest_price(3, f, 0, 2, 0), 500);
     }
 }
