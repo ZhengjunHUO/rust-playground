9e5c0360e84c8c61dbac4fa8cdbc63f83ba616ef
ZhengjunHUO <firelouiszj@hotmail.com>
2023-01-30 13:33:15 +0100

    add write to file procedure
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- web_reqwest/src/main.rs
+++ web_reqwest/src/main.rs
@@ -1,8 +1,11 @@
 use error_chain::error_chain;
 use reqwest;
-use std::io::Read;
+use std::fs::File;
+use std::io::{Read, Write};
 
 const HTTPS_SCHEME: &str = "https://";
+const RESULT_FOLDER: &str = "result/";
+static TARGET_URL: &str = "httpbin.org";
 
 error_chain! {
     foreign_links {
@@ -12,15 +15,23 @@ error_chain! {
 }
 
 fn main() -> Result<()> {
-    let target_url = "www.kernel.org";
-    let mut rslt = reqwest::blocking::get(HTTPS_SCHEME.to_owned() + target_url)?;
+    probe("/get")
+}
+
+fn probe(path: &str) -> Result<()> {
+    let result_filename = RESULT_FOLDER.to_owned() + path;
+    let mut f = File::create(result_filename)?;
+
+    let mut rslt = reqwest::blocking::get(HTTPS_SCHEME.to_owned() + TARGET_URL + path)?;
     let mut body = String::new();
     rslt.read_to_string(&mut body)?;
 
-    println!("Host: {}", target_url);
+    println!("Host: {}", TARGET_URL);
     println!("HTTP/2 {}", rslt.status());
     println!("Headers:\n{:#?}", rslt.headers());
     println!("Body:\n{}", body);
+
+    f.write_all(body.as_bytes())?;
 
     Ok(())
 }
