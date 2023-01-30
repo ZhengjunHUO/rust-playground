d94c0349e1e34a34754cc6529f6c3f43bf803376
ZhengjunHUO <firelouiszj@hotmail.com>
2023-01-30 15:37:39 +0100

    improve read/write from file
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- web_reqwest/src/main.rs
+++ web_reqwest/src/main.rs
@@ -1,11 +1,12 @@
 use error_chain::error_chain;
 use reqwest;
 use std::fs::File;
-use std::io::{Read, Write};
+use std::io::{BufRead, BufReader, Read, Write};
 
 const HTTPS_SCHEME: &str = "https://";
 const RESULT_FOLDER: &str = "result/";
-static TARGET_URL: &str = "httpbin.org";
+static TARGET_URL: &str = "httpbin.org/";
+static EXCLUDE_PATTERN: &str = "Unknown";
 
 error_chain! {
     foreign_links {
@@ -15,23 +16,35 @@ error_chain! {
 }
 
 fn main() -> Result<()> {
-    probe("/get")
+    let f = File::open("urls")?;
+    let buf = BufReader::new(f);
+    for line in buf.lines() {
+        if let Ok(path) = line {
+            match probe(&path) {
+                Err(e) => { println!("Failed to retrieve {}: {}", path, e) }
+                _ => {}
+            }
+        }
+    }
+
+    Ok(())
 }
 
 fn probe(path: &str) -> Result<()> {
-    let result_filename = RESULT_FOLDER.to_owned() + path;
-    let mut f = File::create(result_filename)?;
-
+    println!("[DEBUG] URL: {}{}", TARGET_URL, path);
     let mut rslt = reqwest::blocking::get(HTTPS_SCHEME.to_owned() + TARGET_URL + path)?;
+    println!("[DEBUG] HTTP/2 {}", rslt.status());
     let mut body = String::new();
     rslt.read_to_string(&mut body)?;
+    println!("[DEBUG] Headers:\n{:#?}", rslt.headers());
 
-    println!("Host: {}", TARGET_URL);
-    println!("HTTP/2 {}", rslt.status());
-    println!("Headers:\n{:#?}", rslt.headers());
-    println!("Body:\n{}", body);
-
-    f.write_all(body.as_bytes())?;
+    if !body.contains(EXCLUDE_PATTERN) {
+        let result_filename = RESULT_FOLDER.to_owned() + path;
+        let mut f = File::create(&result_filename)?;
+        f.write_all(body.as_bytes())?;
+        println!("[DEBUG] Write {} to disk.", result_filename);
+    }
+    //println!("Body:\n{}", body);
 
     Ok(())
 }
