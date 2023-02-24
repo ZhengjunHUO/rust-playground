d8f0cb2404725549b3f1f29eee16ae9885dd756a
ZhengjunHUO <firelouiszj@hotmail.com>
2023-02-24 16:53:25 +0100

    cow: put off allocation unless & until it’s really necessary
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- cow/src/main.rs
+++ cow/src/main.rs
@@ -1,5 +1,18 @@
+use rand::{thread_rng, Rng};
 use std::borrow::Cow;
 use std::env::var;
+
+fn affinity<'a>() -> Option<&'a str> {
+    let mut rng = thread_rng();
+    let n: u32 = rng.gen_range(0..10);
+    if n > 7 {
+        return Some("my dear");
+    }
+    if n > 3 {
+        return Some("my friend");
+    }
+    None
+}
 
 fn from_env(s: &str) -> String {
     var(s).unwrap_or("unknown".to_string())
@@ -20,4 +33,12 @@ fn main() {
         from_env_cow("USER"),
         from_env_cow("HOME")
     );
+
+    let mut username = from_env_cow("USER");
+    if let Some(aff) = affinity() {
+        // if username is borrowed, will keep as borrowed until need to be modified
+        username += " ";
+        username.to_mut().push_str(aff);
+    }
+    println!("Welcome {} !", username);
 }
