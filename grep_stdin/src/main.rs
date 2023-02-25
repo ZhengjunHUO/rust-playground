b953ab887c077ee7f52c9eb75a4b6807c6a1b143
ZhengjunHUO <firelouiszj@hotmail.com>
2023-02-25 15:15:04 +0100

    complete the grep program
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- grep_stdin/src/main.rs
+++ grep_stdin/src/main.rs
@@ -1,5 +1,9 @@
-use std::io;
+use std::env;
+use std::error::Error;
+use std::fs::File;
 use std::io::prelude::*;
+use std::io::{self, BufReader};
+use std::path::PathBuf;
 
 fn grep<B>(pattern: &str, br: B) -> io::Result<()>
 where
@@ -15,7 +19,35 @@ where
     Ok(())
 }
 
+fn do_grep() -> Result<(), Box<dyn Error>> {
+    let mut args = env::args().skip(1);
+    // Try get the pattern at args[1]
+    let pattern = match args.next() {
+        Some(p) => p,
+        None => Err("Can't parse the pattern ...")?,
+    };
+
+    let fs = args.map(PathBuf::from).collect::<Vec<PathBuf>>();
+    // If no file is provided in args, try grep pattern from stdin
+    if fs.is_empty() {
+        grep(&pattern, io::stdin().lock())?;
+    } else {
+        for f in fs {
+            let file = File::open(f)?;
+            grep(&pattern, BufReader::new(file))?;
+        }
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("grep from stdin ...");
-    let _ = grep("Rust", io::stdin().lock());
+    let rslt = do_grep();
+    match rslt {
+        Err(e) => {
+            eprintln!("{}", e);
+            std::process::exit(1);
+        }
+        _ => {}
+    }
 }
