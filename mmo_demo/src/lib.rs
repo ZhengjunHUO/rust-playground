7e76491e9171b31c730b28a84ffd12a58cae1846
ZhengjunHUO <firelouiszj@hotmail.com>
2023-03-04 14:25:23 +0100

    add test & expose to public
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- mmo_demo/src/lib.rs
+++ mmo_demo/src/lib.rs
@@ -5,21 +5,21 @@ const ROOM_SIZE: usize = 4;
 type UserId = u32;
 type WaitRoom = Vec<UserId>;
 
-struct AppContext {
+pub struct AppContext {
     // Protect WaitRoom with Mutex to avoid data race
     wait_room: Mutex<WaitRoom>,
     handlers: Mutex<Vec<thread::JoinHandle<()>>>,
 }
 
 impl AppContext {
-    fn server_up() -> Arc<AppContext> {
+    pub fn server_up() -> Arc<AppContext> {
         Arc::new(AppContext {
             wait_room: Mutex::new(vec![]),
             handlers: Mutex::new(vec![]),
         })
     }
 
-    fn enter_room(&self, player: UserId) {
+    pub fn enter_room(&self, player: UserId) {
         // Add new player to room
         let mut g = self.wait_room.lock().unwrap();
         g.push(player);
@@ -36,7 +36,8 @@ impl AppContext {
     }
 }
 
-fn start_game(players: Vec<UserId>) {
+pub fn start_game(players: Vec<UserId>) {
+    println!("{:?}", players);
     // run the game
 }
 
@@ -45,5 +46,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_mmo() {}
+    fn test_mmo() {
+        let ctx = AppContext::server_up();
+        ctx.enter_room(5);
+        ctx.enter_room(3);
+        ctx.enter_room(7);
+        assert_eq!(ctx.wait_room.lock().unwrap().len(), 3);
+        ctx.enter_room(1);
+        assert_eq!(ctx.wait_room.lock().unwrap().len(), 0);
+        assert_eq!(ctx.handlers.lock().unwrap().len(), 1);
+    }
 }
