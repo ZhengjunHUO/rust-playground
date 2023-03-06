3bb0f0cfd61ffd6ee406533d69e50a4bb16a29a0
ZhengjunHUO <firelouiszj@hotmail.com>
2023-03-06 10:34:01 +0100

    test RwLock
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- mmo_demo/src/lib.rs
+++ mmo_demo/src/lib.rs
@@ -1,22 +1,46 @@
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, RwLock};
 use std::thread;
 
 const ROOM_SIZE: usize = 4;
 type UserId = u32;
 type WaitRoom = Vec<UserId>;
 
+struct ServerConfig {
+    version: String,
+    enemy_respawned: bool,
+}
+
+impl ServerConfig {
+    pub fn reload(version: String) -> ServerConfig {
+        ServerConfig {
+            version,
+            enemy_respawned: true,
+        }
+    }
+}
+
 pub struct AppContext {
     // Protect WaitRoom with Mutex to avoid data race
     wait_room: Mutex<WaitRoom>,
     handlers: Mutex<Vec<thread::JoinHandle<()>>>,
+    config: RwLock<ServerConfig>,
 }
 
 impl AppContext {
     pub fn server_up() -> Arc<AppContext> {
-        Arc::new(AppContext {
+        let ctx = Arc::new(AppContext {
             wait_room: Mutex::new(vec![]),
             handlers: Mutex::new(vec![]),
-        })
+            config: RwLock::new(ServerConfig {
+                version: String::from("1.0.0"),
+                enemy_respawned: false,
+            }),
+        });
+        println!(
+            "[DEBUG] Server up, version: {}",
+            ctx.config.read().unwrap().version
+        );
+        ctx
     }
 
     pub fn enter_room(&self, player: UserId) {
@@ -33,6 +57,17 @@ impl AppContext {
                 .unwrap()
                 .push(thread::spawn(move || start_game(ps)));
         }
+    }
+
+    pub fn reload_config(&self, version: String) {
+        let new_conf = ServerConfig::reload(version);
+        let mut g = self.config.write().unwrap();
+        *g = new_conf;
+    }
+
+    pub fn is_enemy_respawned(&self) -> bool {
+        let g = self.config.read().unwrap();
+        g.enemy_respawned
     }
 }
 
@@ -55,5 +90,8 @@ mod tests {
         ctx.enter_room(1);
         assert_eq!(ctx.wait_room.lock().unwrap().len(), 0);
         assert_eq!(ctx.handlers.lock().unwrap().len(), 1);
+        assert!(!ctx.is_enemy_respawned());
+        ctx.reload_config("1.0.1".to_string());
+        assert!(ctx.is_enemy_respawned());
     }
 }
