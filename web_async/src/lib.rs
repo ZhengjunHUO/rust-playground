eafbd76f13948a302a24db908112d3c580486e4d
ZhengjunHUO <firelouiszj@hotmail.com>
2023-03-08 16:33:32 +0100

    debug polling with spawn_local & block_on
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- web_async/src/lib.rs
+++ web_async/src/lib.rs
@@ -1,18 +1,40 @@
 use async_std::io::prelude::*;
-use async_std::net;
+use async_std::{net, task};
 
-pub async fn req_get(host: &str, port: u16, path: &str) -> std::io::Result<String> {
+pub async fn req_get_batch(reqs: Vec<(String, u16, String)>) -> Vec<std::io::Result<String>> {
+    let mut hs = vec![];
+    let mut rslt = vec![];
+
+    for (host, port, path) in reqs {
+        // spawn async fn, collect the handler (future) to a vec
+        hs.push(task::spawn_local(req_get(host, port, path)));
+    }
+
+    // trigger await, then all req will run concurrently
+    for h in hs {
+        rslt.push(h.await);
+    }
+
+    rslt
+}
+
+pub async fn req_get(host: String, port: u16, path: String) -> std::io::Result<String> {
+    eprintln!("[DEBUG] Get {}:{}{}", host, port, path);
     // Open a new connection
-    let mut conn = net::TcpStream::connect((host, port)).await?;
+    let mut conn = net::TcpStream::connect((&*host, port)).await?;
+    eprintln!("[DEBUG] [{}] Connection established!", host);
 
     // Send a Get to destination
     let req = format!("GET {} HTTP/1.1\r\nHost: {}\r\n\r\n", path, host);
     conn.write_all(req.as_bytes()).await?;
+    eprintln!("[DEBUG] [{}] Request sent!", host);
     conn.shutdown(net::Shutdown::Write)?;
+    eprintln!("[DEBUG] [{}] Stop write!", host);
 
-    // Read response to a string 
+    // Read response to a string
     let mut resp = String::new();
     conn.read_to_string(&mut resp).await?;
+    eprintln!("[DEBUG] [{}] Response received!", host);
 
     Ok(resp)
 }
--- web_async/src/main.rs
+++ web_async/src/main.rs
@@ -1,9 +1,26 @@
 use async_std::task;
-use web_async::req_get;
+use web_async::req_get_batch;
 
-fn main() -> std::io::Result<()> {
-    //let rslt = task::block_on(req_get("www.baidu.com", 80, "/"))?;
-    let rslt = task::block_on(req_get("ifconfig.me", 80, "/"))?;
-    println!("Reply from target host:\r\n{}", rslt);
-    Ok(())
+fn main() {
+    // block on single request
+    //let rslt = task::block_on(req_get("ifconfig.me".to_string(), 80, "/".to_string()))?;
+
+    let reqs = vec![
+        ("ifconfig.me".to_string(), 80, "/".to_string()),
+        ("www.baidu.com".to_string(), 80, "/".to_string()),
+        ("wikipedia.com".to_string(), 80, "/".to_string()),
+    ];
+
+    // block on a pool of request
+    // block_on will be notified by the some future when its await is ready,
+    // then poll that future (which is worth polling) until its next await
+    let rslt = task::block_on(req_get_batch(reqs));
+
+    // examine the result
+    for r in rslt {
+        match r {
+            Ok(resp) => println!("**********\r\n{}", resp.len()),
+            Err(e) => eprintln!("Failed to Get: {}", e),
+        }
+    }
 }
