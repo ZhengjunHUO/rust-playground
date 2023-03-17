b253238c501e89ce5ebe5549e4ee2edc97b64d7d
ZhengjunHUO <firelouiszj@hotmail.com>
2023-03-17 16:46:00 +0100

    add comments
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- future_test/src/lib.rs
+++ future_test/src/lib.rs
@@ -4,10 +4,14 @@ use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll, Waker};
 use std::thread;
 
+pub mod executor;
+
 pub struct MyFuture<T>(Arc<Mutex<Partage<T>>>);
 
+// shared beteen future and the thread running the closure in fn spawn_blocking
 struct Partage<T> {
     payload: Option<T>,
+    // used to receive the callback passed by the executor (eg. block_on)
     waker: Option<Waker>,
 }
 
@@ -16,11 +20,15 @@ impl<T: Send> Future for MyFuture<T> {
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<T> {
         let mut g = self.0.lock().unwrap();
+
+        // check if future's payload is ready, return the Partage's payload
         if let Some(content) = g.payload.take() {
             return Poll::Ready(content);
         }
 
+        // if not yet ready, save the callback waker passed by context
         g.waker = Some(cx.waker().clone());
+        // tell the executor that it's not ready, need to poll again later
         Poll::Pending
     }
 }
@@ -33,11 +41,13 @@ where
     F: FnOnce() -> T + Send + 'static,
     T: Send + 'static,
 {
+    // [I] create a rdv point between future and thread
     let rdv = Arc::new(Mutex::new(Partage {
         payload: None,
         waker: None,
     }));
 
+    // [II] spawn a thread dedicated to blocking operations (the closure f)
     thread::spawn({
         // ref counter plus 1
         let rdv_cloned = rdv.clone();
@@ -46,16 +56,20 @@ where
             let rslt = f();
             let waker_or_none = {
                 let mut g = rdv_cloned.lock().unwrap();
+                // (1) save the result returned by closure f
                 g.payload = Some(rslt);
                 g.waker.take()
             };
 
+            // (2) the future now is worth polling again, notify the executor
             if let Some(waker) = waker_or_none {
+                // waker is consumed
                 waker.wake();
             }
         }
     });
 
+    // [III] return the future immediately
     MyFuture(rdv)
 }
 
