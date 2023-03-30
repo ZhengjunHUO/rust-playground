f58cf8cad824dcd07e1e0bf99a3f4f6a2a7e3ee9
ZhengjunHUO <firelouiszj@hotmail.com>
2023-03-30 16:51:38 +0200

    prepare utils
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- git-play/src/main.rs
+++ git-play/src/main.rs
@@ -1,10 +1,12 @@
-pub mod raw;
-
 use crate::raw::*;
+use crate::utils::check;
+
+pub mod raw;
+mod utils;
 
 fn main() {
     unsafe {
-        git_libgit2_init();
+        check("init libgit", git_libgit2_init());
         git_libgit2_shutdown();
     }
 }
---
+++ git-play/src/utils.rs
@@ -0,0 +1,32 @@
+use crate::raw;
+use std::ffi::CStr;
+use std::os::raw::c_int;
+use std::process::exit;
+
+pub fn check(msg: &'static str, exit_status: c_int) -> c_int {
+    if exit_status < 0 {
+        unsafe {
+            let err = &*raw::giterr_last();
+            println!(
+                "Error occurred when {}: {} [{}]",
+                msg,
+                CStr::from_ptr(err.message).to_string_lossy(),
+                err.klass
+            );
+            exit(1);
+        }
+    }
+
+    exit_status
+}
+
+unsafe fn print_commit(commit: *const raw::git_commit) {
+    let author = raw::git_commit_author(commit);
+    let name = CStr::from_ptr((*author).name).to_string_lossy();
+    let email = CStr::from_ptr((*author).email).to_string_lossy();
+
+    let msg = raw::git_commit_message(commit);
+    let content = CStr::from_ptr(msg).to_string_lossy();
+
+    println!("Author: {} <{}>\n{}\n", name, email, content);
+}
