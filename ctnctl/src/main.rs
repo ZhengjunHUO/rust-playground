1bbe5280eecb6445892698496560dd3f3561f7c2
ZhengjunHUO <firelouiszj@hotmail.com>
2023-05-02 14:23:14 +0200

    load & attach

--- ctnctl/src/main.rs
+++ ctnctl/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    // TO IMPLEMENT
+mod firewall {
+    include!(concat!(env!("OUT_DIR"), "/cgroup_fw.skel.rs"));
+}
+
+use anyhow::{bail, Result};
+use firewall::*;
+use libc;
+
+fn increase_rlimit() -> Result<()> {
+    let rl = libc::rlimit {
+        rlim_cur: 1 << 20,
+        rlim_max: 1 << 20,
+    };
+
+    if unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rl) } != 0 {
+        bail!("Error increasing rlimit");
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let builder = CgroupFwSkelBuilder::default();
+    increase_rlimit()?;
+    let open = builder.open()?;
+    let mut obj = open.load()?;
+    obj.attach()?;
+    println!("Ready!");
+
+    // Add logic here
+
+    Ok(())
 }
