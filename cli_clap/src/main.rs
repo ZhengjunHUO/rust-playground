fa5bb2aeebea4d6893a66242da2124f346a55f58
ZhengjunHUO <firelouiszj@hotmail.com>
2023-05-19 22:51:18 +0200

    test derive parser #2
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- cli_clap/src/main.rs
+++ cli_clap/src/main.rs
@@ -2,17 +2,21 @@ use clap::{Parser, Subcommand};
 use std::path::PathBuf;
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = None)]
+#[command(name = "TestCli")]
+#[command(author = "ZhengjunHUO <firelouiszj@hotmail.com>")]
+#[command(version = "0.1.0")]
+#[command(about = "explore clap", long_about = None)]
 struct Cmd {
     // 子命令
     #[command(subcommand)]
     subcmd: Option<SubCmd>,
 
     // 识别为带value的Option
-    #[arg(short, long, value_name = "/PATH/TO/CONFIG")]
+    // 只接受--config (long)
+    #[arg(long, value_name = "/PATH/TO/CONFIG")]
     config: Option<PathBuf>,
 
-    // 识别为不带value的Option
+    // 类别u8加上Count, 识别为不带value的Option
     // 统计出现次数
     #[arg(short, long, action = clap::ArgAction::Count)]
     verbose: u8,
@@ -24,13 +28,22 @@ struct Cmd {
 #[derive(Subcommand)]
 enum SubCmd {
     Valid {
-        // 识别为不带value的Option
+        // 类别为布尔值，识别为不带value的Option
         #[arg(short, long)]
         all: bool,
+
+        // 类别为字符串，识别为带value的Option
+        #[arg(long)]
+        inline: String,
+    },
+    Override {
+        // 类别为Vec, 可以接受数个Arguments
+        delta: Vec<PathBuf>,
     },
 }
 
-// eg. ./target/debug/cli_clap -vvv -c /etc/hosts xxx valid -a
+// eg. ./target/debug/cli_clap -vvv --config /etc/hosts huo valid -a --inline never
+// eg. ./target/debug/cli_clap override d1.conf d2.conf d3.conf
 fn main() {
     let cmd = Cmd::parse();
 
@@ -46,12 +59,15 @@ fn main() {
     }
 
     match &cmd.subcmd {
-        Some(SubCmd::Valid { all }) => {
+        Some(SubCmd::Valid { all, inline }) => {
             if *all {
-                println!("[Debug] Valid all !");
+                println!("[Debug] Valid all, inline: {} !", inline);
             } else {
-                println!("[Debug] Valid by default !");
+                println!("[Debug] Valid by default, inline: {}!", inline);
             }
+        }
+        Some(SubCmd::Override { delta }) => {
+            println!("[Debug] Delta files: {:?}", delta);
         }
         None => {}
     }
