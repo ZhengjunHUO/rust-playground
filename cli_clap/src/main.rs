0dadc06ca7e9a5d8e2cf4fc9d44c87f299f77fcd
ZhengjunHUO <firelouiszj@hotmail.com>
2023-05-20 18:40:08 +0200

    add parser validate
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- cli_clap/src/main.rs
+++ cli_clap/src/main.rs
@@ -1,5 +1,8 @@
 use clap::{Args, Parser, Subcommand, ValueEnum};
+use std::ops::RangeInclusive;
 use std::path::PathBuf;
+
+const PORT_RANGE: RangeInclusive<usize> = 1001..=65535;
 
 #[derive(Parser)]
 #[command(name = "TestCli")]
@@ -46,12 +49,20 @@ enum SubCmd {
     Deploy {
         #[arg(short, long, value_enum)]
         infra: Infra,
+
+        /// Tcp port the app will listen on
+        #[arg(short, long, default_value_t = 8080, value_parser = clap::value_parser!(u16).range(1001..))]
+        tcp_port: u16,
+
+        /// Udp port the app will listen on
+        #[arg(short, long, default_value_t = 8088, value_parser = validate_port)]
+        udp_port: u16,
     },
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 enum Infra {
-    K8s,
+    K8S,
     Baremetal,
 }
 
@@ -88,13 +99,32 @@ fn main() {
         Some(SubCmd::Override(oa)) => {
             println!("[Debug] Delta files: {:?}", oa.delta);
         }
-        Some(SubCmd::Deploy { infra }) => {
+        Some(SubCmd::Deploy {
+            infra,
+            tcp_port,
+            udp_port,
+        }) => {
             println!("[Debug] Will deployed to: {:?}", infra);
+            println!("[Debug] Will listen on tcp port {:?}", tcp_port);
+            println!("[Debug] Will listen on udp port {:?}", udp_port);
         }
         None => {}
     }
 
     if let Some(target) = cmd.target.as_deref() {
         println!("[Debug] Target: {}", target);
+    }
+}
+
+fn validate_port(s: &str) -> Result<u16, String> {
+    let p: usize = s.parse().map_err(|_| format!("{} is not a number", s))?;
+    if PORT_RANGE.contains(&p) {
+        Ok(p as u16)
+    } else {
+        Err(format!(
+            "Port should between [{}-{}]",
+            PORT_RANGE.start(),
+            PORT_RANGE.end()
+        ))
     }
 }
