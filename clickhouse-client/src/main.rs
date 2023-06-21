ca815b7858116bdd950a739b3f069dd51041e5b6
ZhengjunHUO <firelouiszj@hotmail.com>
2023-06-21 18:43:56 +0200

    test CRUD with ckh client
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- clickhouse-client/src/main.rs
+++ clickhouse-client/src/main.rs
@@ -1,24 +1,49 @@
 use anyhow::Result;
-use clickhouse::Client;
-use clickhouse::Row;
-use serde::Deserialize;
+use clickhouse::{Client, Row};
+use serde::{Deserialize, Serialize};
+use serde_repr::{Deserialize_repr, Serialize_repr};
+use std::time::UNIX_EPOCH;
 use tokio::runtime::Runtime;
 
 #[derive(Row, Deserialize)]
 #[allow(non_snake_case)]
+#[allow(dead_code)]
 struct MyRow<'a> {
     level: &'a str,
     className: &'a str,
 }
 
-fn main() -> Result<()> {
-    let client = Client::default()
-        .with_url("http://ckh.huo.io:80")
-        .with_user("rafal")
-        .with_password("thisIsDevPassword")
-        .with_database("default");
+#[derive(Debug, Serialize, Deserialize, Row)]
+struct Entree {
+    timestamp: u64,
+    message: String,
+    level: Level,
+}
 
+#[derive(Debug, Serialize_repr, Deserialize_repr)]
+#[repr(u8)]
+enum Level {
+    Debug = 1,
+    Info = 2,
+    Warn = 3,
+    Error = 4,
+}
+
+fn now() -> u64 {
+    UNIX_EPOCH
+        .elapsed()
+        .expect("Error retrieving system time")
+        .as_nanos() as u64
+}
+
+fn main() -> Result<()> {
+    let client = Client::default().with_url("http://ckh.huo.io:80");
+    //        .with_user("rafal")
+    //        .with_password("thisIsDevPassword")
+    //        .with_database("default");
     let rt = Runtime::new().unwrap();
+
+    /* select query
     let mut cursor = client
         .query("SELECT ?fields FROM rafal_logging")
         .fetch::<MyRow<'_>>()?;
@@ -27,6 +52,55 @@ fn main() -> Result<()> {
         while let Some(row) = cursor.next().await? {
             println!("Got level: {}; classname: {}", row.level, row.className);
         }
+        Ok(())
+    })
+    */
+
+    rt.block_on(async {
+        println!("[DEBUG] Drop table if exsit ...");
+        client
+            .query("DROP TABLE IF EXISTS daily_log")
+            .execute()
+            .await?;
+
+        println!("[DEBUG] Create table ...");
+        client
+            .query(
+                "
+                    CREATE TABLE IF NOT EXISTS daily_log (
+                        timestamp       DateTime64(9),
+                        message         String,
+                        level           Enum8(
+                                            'Debug' = 1,
+                                            'Info' = 2,
+                                            'Warn' = 3,
+                                            'Error' = 4
+                                        )
+                    )
+                    ENGINE = Memory
+                ",
+            )
+            .execute()
+            .await?;
+
+        println!("[DEBUG] Insert into table ...");
+        let mut insert = client.insert("daily_log")?;
+        insert
+            .write(&Entree {
+                timestamp: now(),
+                message: "Hello from rust".to_string(),
+                level: Level::Info,
+            })
+            .await?;
+        insert.end().await?;
+
+        println!("[DEBUG] Check table after insert ...");
+        let entrees = client
+            .query("SELECT ?fields FROM daily_log")
+            .fetch_all::<Entree>()
+            .await?;
+        println!("Got: {:?}", entrees);
+
         Ok(())
     })
 }
