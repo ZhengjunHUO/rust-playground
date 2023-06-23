d90a4bb35d9c480721bc175fc706f46878e2976b
ZhengjunHUO <firelouiszj@hotmail.com>
2023-06-23 14:14:15 +0200

    refacto ckh client test code
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- clickhouse-client/src/main.rs
+++ clickhouse-client/src/main.rs
@@ -36,76 +36,69 @@ fn now() -> u64 {
         .as_nanos() as u64
 }
 
-fn main() -> Result<()> {
-    let client = Client::default().with_url("http://ckh.huo.io:80");
-    //        .with_user("rafal")
-    //        .with_password("thisIsDevPassword")
-    //        .with_database("default");
-    let rt = Runtime::new().unwrap();
-
-    /* select query
+async fn select(client: Client) -> Result<()> {
+    // select query
     let mut cursor = client
         .query("SELECT ?fields FROM rafal_logging")
         .fetch::<MyRow<'_>>()?;
 
-    rt.block_on(async {
-        while let Some(row) = cursor.next().await? {
-            println!("Got level: {}; classname: {}", row.level, row.className);
-        }
-        Ok(())
-    })
-    */
+    while let Some(row) = cursor.next().await? {
+        println!("Got level: {}; classname: {}", row.level, row.className);
+    }
+    Ok(())
+}
 
+async fn crud(client: Client) -> Result<()> {
     // CRUD
-    rt.block_on(async {
-        println!("[DEBUG] Drop table if exsit ...");
-        client
-            .query("DROP TABLE IF EXISTS daily_log")
-            .execute()
-            .await?;
+    println!("[DEBUG] Drop table if exsit ...");
+    client
+        .query("DROP TABLE IF EXISTS daily_log")
+        .execute()
+        .await?;
 
-        println!("[DEBUG] Create table ...");
-        client
-            .query(
-                "
-                    CREATE TABLE IF NOT EXISTS daily_log (
-                        timestamp       DateTime64(9),
-                        message         String,
-                        level           Enum8(
-                                            'Debug' = 1,
-                                            'Info' = 2,
-                                            'Warn' = 3,
-                                            'Error' = 4
-                                        )
-                    )
-                    ENGINE = Memory
-                ",
-            )
-            .execute()
-            .await?;
+    println!("[DEBUG] Create table ...");
+    client
+        .query(
+            "
+                CREATE TABLE IF NOT EXISTS daily_log (
+                    timestamp       DateTime64(9),
+                    message         String,
+                    level           Enum8(
+                                        'Debug' = 1,
+                                        'Info' = 2,
+                                        'Warn' = 3,
+                                        'Error' = 4
+                                    )
+                )
+                ENGINE = Memory
+            ",
+        )
+        .execute()
+        .await?;
 
-        println!("[DEBUG] Insert into table ...");
-        let mut insert = client.insert("daily_log")?;
-        insert
-            .write(&Entree {
-                timestamp: now(),
-                message: "Hello from rust".to_string(),
-                level: Level::Info,
-            })
-            .await?;
-        insert.end().await?;
+    println!("[DEBUG] Insert into table ...");
+    let mut insert = client.insert("daily_log")?;
+    insert
+        .write(&Entree {
+            timestamp: now(),
+            message: "Hello from rust".to_string(),
+            level: Level::Info,
+        })
+        .await?;
+    insert.end().await?;
 
-        println!("[DEBUG] Check table after insert ...");
-        let entrees = client
-            .query("SELECT ?fields FROM daily_log")
-            .fetch_all::<Entree>()
-            .await?;
-        println!("Got: {:?}", entrees);
+    println!("[DEBUG] Check table after insert ...");
+    let entrees = client
+        .query("SELECT ?fields FROM daily_log")
+        .fetch_all::<Entree>()
+        .await?;
+    println!("Got: {:?}", entrees);
 
-        Ok(())
-    })
+    Ok(())
+}
 
-    /* restoration
+async fn restore(client: Client) -> Result<()> {
+    // restoration
     let bucket_url = "https://storage.googleapis.com/test-ckh-backup/mtms-d1/";
     let access_key = std::env::var("GCS_ACCESS_KEY")?;
     let secret_key = std::env::var("GCS_SECRET_KEY")?;
@@ -114,11 +107,18 @@ fn main() -> Result<()> {
         bucket_url, access_key, secret_key
     );
 
-    rt.block_on(async {
-        println!("[DEBUG] Restore table ...");
-        client.query(&restore_query).execute().await?;
+    println!("[DEBUG] Restore table ...");
+    client.query(&restore_query).execute().await?;
 
-        Ok(())
-    })
-    */
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let client = Client::default().with_url("http://ckh.huo.io:80");
+    //        .with_user("rafal")
+    //        .with_password("thisIsDevPassword")
+    //        .with_database("default");
+    let rt = Runtime::new().unwrap();
+
+    rt.block_on(async { crud(client).await })
 }
