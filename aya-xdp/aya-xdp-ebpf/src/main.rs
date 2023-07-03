b423b02ed2ace47516f3c856f16484c48bad254d
ZhengjunHUO <firelouiszj@hotmail.com>
2023-07-03 06:44:04 +0000

    update ebpf prog to parse pkt
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- aya-xdp/aya-xdp-ebpf/src/main.rs
+++ aya-xdp/aya-xdp-ebpf/src/main.rs
@@ -3,6 +3,13 @@
 
 use aya_bpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
 use aya_log_ebpf::info;
+use core::mem;
+use network_types::{
+    eth::{EthHdr, EtherType},
+    ip::{IpProto, Ipv4Hdr},
+    tcp::TcpHdr,
+    udp::UdpHdr,
+};
 
 #[xdp(name = "aya_xdp")]
 pub fn aya_xdp(ctx: XdpContext) -> u32 {
@@ -12,9 +19,49 @@ pub fn aya_xdp(ctx: XdpContext) -> u32 {
     }
 }
 
-fn try_aya_xdp(ctx: XdpContext) -> Result<u32, u32> {
-    info!(&ctx, "received a packet");
+fn try_aya_xdp(ctx: XdpContext) -> Result<u32, ()> {
+    let ethhdr: *const EthHdr = ptr_at(&ctx, 0)?;
+    match unsafe { (*ethhdr).ether_type } {
+        EtherType::Ipv4 => {}
+        _ => return Ok(xdp_action::XDP_PASS),
+    }
+
+    let ipv4hdr: *const Ipv4Hdr = ptr_at(&ctx, EthHdr::LEN)?;
+    let source_addr = u32::from_be(unsafe { (*ipv4hdr).src_addr });
+
+    let source_port = match unsafe { (*ipv4hdr).proto } {
+        IpProto::Tcp => {
+            let tcphdr: *const TcpHdr =
+                ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
+            u16::from_be(unsafe { (*tcphdr).source })
+        }
+        IpProto::Udp => {
+            let udphdr: *const UdpHdr =
+                ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
+            u16::from_be(unsafe { (*udphdr).source })
+        }
+        _ => return Err(()),
+    };
+
+    info!(
+        &ctx,
+        "SRC IP: {:i}, SRC PORT: {}", source_addr, source_port
+    );
+
     Ok(xdp_action::XDP_PASS)
+}
+
+#[inline(always)]
+fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+    let len = mem::size_of::<T>();
+
+    if start + offset + len > end {
+        return Err(());
+    }
+
+    Ok((start + offset) as *const T)
 }
 
 #[panic_handler]
