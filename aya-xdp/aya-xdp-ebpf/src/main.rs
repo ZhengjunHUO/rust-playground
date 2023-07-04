737f56248f1c5e59bae2e71428c57e230e6bab0e
ZhengjunHUO <firelouiszj@hotmail.com>
2023-07-04 18:31:15 +0000

    retrieve more info from ctx
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- aya-xdp/aya-xdp-ebpf/src/main.rs
+++ aya-xdp/aya-xdp-ebpf/src/main.rs
@@ -28,24 +28,31 @@ fn try_aya_xdp(ctx: XdpContext) -> Result<u32, ()> {
 
     let ipv4hdr: *const Ipv4Hdr = ptr_at(&ctx, EthHdr::LEN)?;
     let source_addr = u32::from_be(unsafe { (*ipv4hdr).src_addr });
+    let dest_addr = u32::from_be(unsafe { (*ipv4hdr).dst_addr });
+    let dest_port;
+    let proto;
 
     let source_port = match unsafe { (*ipv4hdr).proto } {
         IpProto::Tcp => {
             let tcphdr: *const TcpHdr =
                 ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
+            dest_port = u16::from_be(unsafe { (*tcphdr).dest });
+            proto = "TCP";
             u16::from_be(unsafe { (*tcphdr).source })
         }
         IpProto::Udp => {
             let udphdr: *const UdpHdr =
                 ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
+            dest_port = u16::from_be(unsafe { (*udphdr).dest });
+            proto = "UDP";
             u16::from_be(unsafe { (*udphdr).source })
         }
-        _ => return Err(()),
+        _ => return Ok(xdp_action::XDP_PASS),
     };
 
     info!(
         &ctx,
-        "SRC IP: {:i}, SRC PORT: {}", source_addr, source_port
+        "{} in {:i}:{} > {:i}:{}", proto, source_addr, source_port, dest_addr, dest_port
     );
 
     Ok(xdp_action::XDP_PASS)
