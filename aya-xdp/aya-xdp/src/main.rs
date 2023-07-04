7a42191f2dc9b1e63b33ace9845d1ee7deda369a
ZhengjunHUO <firelouiszj@hotmail.com>
2023-07-04 19:56:30 +0000

    persist xdp prog after userspace prog exit
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- aya-xdp/aya-xdp/src/main.rs
+++ aya-xdp/aya-xdp/src/main.rs
@@ -1,10 +1,9 @@
 use anyhow::Context;
 use aya::programs::{Xdp, XdpFlags};
-use aya::{include_bytes_aligned, Bpf};
+use aya::{include_bytes_aligned, BpfLoader, Btf};
 use aya_log::BpfLogger;
 use clap::Parser;
-use log::{info, warn};
-use tokio::signal;
+use log::warn;
 
 #[derive(Debug, Parser)]
 struct Opt {
@@ -18,18 +17,20 @@ async fn main() -> Result<(), anyhow::Error> {
 
     env_logger::init();
 
-    // This will include your eBPF object file as raw bytes at compile-time and load it at
-    // runtime. This approach is recommended for most real-world use cases. If you would
-    // like to specify the eBPF program at runtime rather than at compile-time, you can
-    // reach for `Bpf::load_file` instead.
     #[cfg(debug_assertions)]
-    let mut bpf = Bpf::load(include_bytes_aligned!(
-        "../../target/bpfel-unknown-none/debug/aya-xdp"
-    ))?;
+    let mut bpf = BpfLoader::new()
+        .btf(Btf::from_sys_fs().ok().as_ref())
+        .map_pin_path("/sys/fs/bpf/aya-xdp")
+        .load(include_bytes_aligned!(
+            "../../target/bpfel-unknown-none/debug/aya-xdp"
+        ))?;
     #[cfg(not(debug_assertions))]
-    let mut bpf = Bpf::load(include_bytes_aligned!(
-        "../../target/bpfel-unknown-none/release/aya-xdp"
-    ))?;
+    let mut bpf = BpfLoader::new()
+        .btf(Btf::from_sys_fs().ok().as_ref())
+        .map_pin_path("/sys/fs/bpf/aya-xdp")
+        .load(include_bytes_aligned!(
+            "../../target/bpfel-unknown-none/release/aya-xdp"
+        ))?;
     if let Err(e) = BpfLogger::init(&mut bpf) {
         // This can happen if you remove all log statements from your eBPF program.
         warn!("failed to initialize eBPF logger: {}", e);
@@ -38,10 +39,7 @@ async fn main() -> Result<(), anyhow::Error> {
     program.load()?;
     program.attach(&opt.iface, XdpFlags::default())
         .context("failed to attach the XDP program with default flags - try changing XdpFlags::default() to XdpFlags::SKB_MODE")?;
-
-    info!("Waiting for Ctrl-C...");
-    signal::ctrl_c().await?;
-    info!("Exiting...");
+    program.pin("/sys/fs/bpf/aya-xdp-prog")?;
 
     Ok(())
 }
