c3906cf7807efa297f0de4377ab92bf1318cb880
ZhengjunHUO <firelouiszj@hotmail.com>
2023-07-12 11:24:36 +0200

    test regex
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

---
+++ regex_test/src/lib.rs
@@ -0,0 +1,43 @@
+use anyhow::Result;
+use regex::Regex;
+use std::collections::hash_set::HashSet;
+
+pub fn filter_tables(raw: HashSet<String>, filters: Vec<String>) -> Result<HashSet<String>> {
+    let mut rslt = HashSet::new();
+
+    for filter in filters {
+        let rx = Regex::new(&filter)?;
+        let matched: HashSet<String> = raw
+            .iter()
+            .filter(|n| rx.is_match(n))
+            .map(|s| s.to_owned())
+            .collect();
+        rslt = rslt.union(&matched).map(|s| s.to_owned()).collect();
+    }
+
+    Ok(rslt)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_macro::vec_string;
+
+    #[test]
+    fn test_filter_tables() {
+        assert_eq!(
+            filter_tables(
+                HashSet::from_iter(vec_string![
+                    "default",
+                    "shard_foo",
+                    "shard_bar",
+                    "monitoring",
+                    "foobar"
+                ]),
+                vec_string!["^shard_.*", ".*foo.*"]
+            )
+            .unwrap(),
+            HashSet::from_iter(vec_string!["shard_foo", "foobar", "shard_bar"])
+        );
+    }
+}
