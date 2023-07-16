6e05fcce596b7e07aaa97b72f4a922591ff42cda
ZhengjunHUO <firelouiszj@hotmail.com>
2023-07-16 10:56:10 +0200

    test std process
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

---
+++ process_test/src/main.rs
@@ -0,0 +1,47 @@
+use std::collections::HashMap;
+use std::env;
+use std::process::{Command, Stdio};
+
+fn main() {
+    print_filtered_envs();
+    piped_cmd();
+}
+
+fn print_filtered_envs() {
+    let filtered_env: HashMap<String, String> = env::vars()
+        .filter(|&(ref k, _)| k == "USER" || k == "HOME" || k == "PATH")
+        .collect();
+
+    Command::new("printenv")
+        .stdin(Stdio::null())
+        .stdout(Stdio::inherit())
+        .env_clear()
+        .envs(&filtered_env)
+        .spawn()
+        .expect("printenv failed to start");
+}
+
+fn piped_cmd() {
+    println!("{}", env::current_dir().unwrap().display());
+    let find = Command::new("find")
+        .arg("./src")
+        .arg("-type")
+        .arg("f")
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to exec find");
+
+    let find_out = find.stdout.expect("Failed to get find's stdout");
+
+    let grep = Command::new("grep")
+        .arg(".*rs")
+        .stdin(Stdio::from(find_out))
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to exec grep process");
+
+    let out = grep
+        .wait_with_output()
+        .expect("Failed to wait grep's output");
+    println!("Result: {}", String::from_utf8_lossy(&out.stdout));
+}
