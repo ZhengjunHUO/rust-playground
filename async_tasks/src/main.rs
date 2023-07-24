52137ac5cb8f3c14575f8ba84086530373951270
ZhengjunHUO <firelouiszj@hotmail.com>
2023-07-24 10:44:20 +0200

    use channel to collect logs
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- async_tasks/src/main.rs
+++ async_tasks/src/main.rs
@@ -1,11 +1,14 @@
-use rand::Rng;
+use rand::rngs::StdRng;
+use rand::{Rng, SeedableRng};
 use std::collections::VecDeque;
 use std::env;
 use std::fs::read_to_string;
 use std::sync::{Arc, Mutex};
 use std::{thread, time};
+use tokio::sync::mpsc::{channel, Sender};
 
-async fn handle(client_id: String, table_list: TableList) -> String {
+async fn handle(idx: u8, client_id: String, table_list: TableList, tx: Sender<String>) -> String {
+    println!("ckh-{}: Worker start !", client_id);
     loop {
         let table;
 
@@ -14,19 +17,62 @@ async fn handle(client_id: String, table_list: TableList) -> String {
             table = garde.pop_front();
         }
 
+        let seed = [
+            1, 0, 0, 0, 23, 0, 0, 0, 200, 1, 0, 0, 210, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, idx,
+        ];
+        let mut rng = StdRng::from_seed(seed);
+
         match table {
             Some(table_name) => {
-                let mut rng = rand::thread_rng();
-                let secs = rng.gen_range(3..=20);
+                let secs = rng.gen_range(3..=8);
+                if let Err(e) = tx
+                    .send(format!(
+                        "[ckh-{}] Dealing with the table {}, estimate done in {} secs",
+                        client_id, table_name, secs
+                    ))
+                    .await
+                {
+                    println!(
+                        "[WARN] ckh-{}: Send message with error returned: {}",
+                        client_id, e
+                    );
+                };
+                /*
                 println!(
                     "[ckh-{}] Dealing with the table {}, estimate done in {} secs",
                     client_id, table_name, secs
                 );
+                */
                 thread::sleep(time::Duration::from_secs(secs));
-                println!("[ckh-{}] Done with the table {}", client_id, table_name);
+                if let Err(e) = tx
+                    .send(format!(
+                        "[ckh-{}] Done with the table {}",
+                        client_id, table_name
+                    ))
+                    .await
+                {
+                    println!(
+                        "[WARN] ckh-{}: Send message with error returned: {}",
+                        client_id, e
+                    );
+                };
+                //println!("[ckh-{}] Done with the table {}", client_id, table_name);
             }
             None => {
-                println!("[ckh-{}] Todo list is empty, mission complete", client_id);
+                if let Err(e) = tx
+                    .send(format!(
+                        "[ckh-{}] Todo list is empty, mission complete",
+                        client_id
+                    ))
+                    .await
+                {
+                    println!(
+                        "[WARN] ckh-{}: Send message with error returned: {}",
+                        client_id, e
+                    );
+                };
+                //println!("[ckh-{}] Todo list is empty, mission complete", client_id);
                 break;
             }
         }
@@ -54,16 +100,36 @@ async fn main() {
         .map(|&s| s.into())
         .collect();
     let mut tasks = Vec::with_capacity(eps.len());
-    for ep in eps {
+
+    let (tx, mut rx) = channel(8);
+    let mut senders = Vec::with_capacity(eps.len());
+    for _ in 0..eps.len() - 1 {
+        senders.push(tx.clone());
+    }
+    senders.push(tx);
+
+    for (i, ep) in eps.into_iter().enumerate() {
         let list = tables.clone();
-        tasks.push(tokio::spawn(handle(ep, list)));
+        tasks.push(tokio::spawn(handle(
+            i as u8,
+            ep,
+            list,
+            senders.pop().unwrap(),
+        )));
     }
 
+    /*
     let mut rslt = Vec::with_capacity(tasks.len());
     for task in tasks {
         rslt.push(task.await.unwrap());
     }
+    */
 
-    println!("{:?}", rslt);
-    println!("All done!");
+    println!("[main] Receiving message !");
+    while let Some(msg) = rx.recv().await {
+        println!("{}", msg);
+    }
+
+    //println!("{:?}", rslt);
+    println!("[main] All done!");
 }
