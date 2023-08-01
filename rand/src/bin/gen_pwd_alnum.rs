6eca60df6ac17711faf3cc4f9d4edaa5131912c9
ZhengjunHUO <firelouiszj@hotmail.com>
2023-08-01 14:32:32 +0200

    add color and style
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- rand/src/bin/gen_pwd_alnum.rs
+++ rand/src/bin/gen_pwd_alnum.rs
@@ -1,3 +1,4 @@
+use ansi_term::{Colour, Style};
 use rand::distributions::{Alphanumeric, DistString};
 use rand::{thread_rng, Rng};
 
@@ -11,20 +12,28 @@ fn main() {
     let mut rng = thread_rng();
 
     // 1) based on DistString trait implemented by Alphanumeric
-    println!("Approach 1: {}", Alphanumeric.sample_string(&mut rng, size));
+    println!(
+        "{}: {}",
+        Colour::Blue.paint("Approach 1"),
+        Alphanumeric.sample_string(&mut rng, size)
+    );
     // 2) based on Rng: RngCore trait implemented by ThreadRng
     println!(
-        "Approach 2: {}",
+        "{}: {}",
+        Colour::Red.bold().paint("Approach 2"),
         (0..size)
             .map(|_| rng.sample(Alphanumeric) as char)
             .collect::<String>()
     );
     // 3) also based on Rng: RngCore trait implemented by ThreadRng
     println!(
-        "Approach 3: {}",
-        rng.sample_iter(Alphanumeric)
-            .take(size)
-            .map(char::from)
-            .collect::<String>()
+        "{}: {}",
+        Colour::Cyan.italic().paint("Approach 3"),
+        Style::new().blink().paint(
+            rng.sample_iter(Alphanumeric)
+                .take(size)
+                .map(char::from)
+                .collect::<String>()
+        )
     );
 }
