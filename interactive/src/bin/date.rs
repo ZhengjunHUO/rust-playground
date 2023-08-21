b94031a9d764c13fea069cafa08408177196c292
ZhengjunHUO <firelouiszj@hotmail.com>
2023-08-21 11:27:55 +0200

    test chrono date
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- interactive/src/bin/date.rs
+++ interactive/src/bin/date.rs
@@ -1,4 +1,6 @@
 fn main() {
     let today = chrono::Utc::now().naive_utc().date();
+    let yesterday = today - chrono::Duration::days(1);
     println!("Today is {}", today.format("%Y%m%d"));
+    println!("Yesterday is {}", yesterday.format("%Y%m%d"));
 }
