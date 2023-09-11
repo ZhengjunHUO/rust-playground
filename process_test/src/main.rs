432549d9adc532075d895ed35408a3722e187901
ZhengjunHUO <firelouiszj@hotmail.com>
2023-09-11 14:33:26 +0200

    test glob to grep wildcard files
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- process_test/src/main.rs
+++ process_test/src/main.rs
@@ -1,3 +1,6 @@
+#![allow(dead_code)]
+
+use glob::glob;
 use std::collections::HashMap;
 use std::env;
 use std::fs::File;
@@ -6,7 +9,23 @@ use std::process::{Command, Stdio};
 fn main() {
     //print_filtered_envs();
     //piped_cmd();
-    save_to_file();
+    //save_to_file();
+
+    // Attention: the pattern arg should be quoted: "/path/to/**/*.suffix"
+    let arg = env::args().nth(1);
+    match arg.as_deref() {
+        //Some(p) => list_files_wildcard(p),
+        Some(p) => {
+            for entry in glob(p).expect("Failed to parse pattern") {
+                match entry {
+                    Ok(path) => println!("Found: {}", path.display()),
+                    Err(e) => println!("Error occurred: {}", e),
+                }
+            }
+        }
+
+        _ => eprintln!("Wait for a path"),
+    }
 }
 
 fn print_filtered_envs() {
@@ -59,4 +78,15 @@ fn save_to_file() {
         .stdout(stdio)
         .spawn()
         .expect("echo failed to exec");
+}
+
+fn list_files_wildcard(wildcard: &str) {
+    let ls_cmd = Command::new("ls")
+        .arg(wildcard)
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to exec ls");
+    let out = ls_cmd.wait_with_output().expect("Failed to wait ls output");
+    // print only the first match ??
+    println!("Result: {}", String::from_utf8_lossy(&out.stdout));
 }
