2100f9a9b50e6ab3a9a5593aa7012f35fa95cacf
ZhengjunHUO <firelouiszj@hotmail.com>
2023-12-21 20:27:50 +0100

    mix post and get req
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- web_warp/src/bin/post.rs
+++ web_warp/src/bin/post.rs
@@ -19,15 +19,27 @@ async fn main() {
         (String::from("fufu"), 0),
     ])));
 
+    let cand_vote = candidates.clone();
+    // curl -X POST -H "Content-Type: application/json" -d '{"name": "huo","votes": 1}' 127.0.0.1:8000/vote
     let vote = warp::post()
         .and(warp::path!("vote"))
         .and(warp::body::content_length_limit(4096))
         .and(warp::body::json())
-        .map(move |payload: Candidate| {
-            update_candidate(&payload.name, payload.votes, candidates.clone())
+        .and(warp::header("user-agent"))
+        .map(move |payload: Candidate, agent: String| {
+            println!("[DEBUG] Receive votes from agent {}", agent);
+            update_candidate(&payload.name, payload.votes, cand_vote.clone())
         });
 
-    warp::serve(vote).run(([127, 0, 0, 1], 8000)).await;
+    let cand_show = candidates.clone();
+    // curl 127.0.0.1:8000/show
+    let show = warp::get()
+        .and(warp::path!("show"))
+        .map(move || print_all(cand_show.clone()));
+
+    let routes = vote.or(show);
+
+    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
 }
 
 fn update_candidate(name: &str, votes: u32, cands: CandidateList) -> String {
@@ -37,5 +49,13 @@ fn update_candidate(name: &str, votes: u32, cands: CandidateList) -> String {
         .and_modify(|sum| *sum += votes)
         .or_insert(votes);
     println!("[DEBUG] {:?}", guard);
-    format!("{} got {} vote(s) !", name, *guard.get(name).unwrap())
+    format!("{} got {} vote(s) !\n", name, *guard.get(name).unwrap())
+}
+
+fn print_all(cands: CandidateList) -> String {
+    let guard = cands.lock().unwrap();
+    guard
+        .iter()
+        .map(|(name, sum)| format!("{} currently has {} vote(s) !\n", name, sum))
+        .collect::<String>()
 }
