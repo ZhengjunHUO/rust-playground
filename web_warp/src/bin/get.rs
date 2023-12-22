2de9471866d3af243f33ab327eb8e20805bf6c9d
ZhengjunHUO <firelouiszj@hotmail.com>
2023-12-22 11:03:34 +0100

    explore error handling
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- web_warp/src/bin/get.rs
+++ web_warp/src/bin/get.rs
@@ -1,4 +1,9 @@
-use warp::{path, Filter};
+use warp::{http::StatusCode, path, reject, reply, Filter, Rejection, Reply};
+
+#[derive(Debug)]
+struct IllegalInput;
+
+impl reject::Reject for IllegalInput {}
 
 #[tokio::main]
 async fn main() {
@@ -10,12 +15,41 @@ async fn main() {
         .map(|| "You can call /huo/<YOUR_NAME>/<YOUR_LOCATION>!\n");
 
     // curl 127.0.0.1:8000/huo/fufu/paris
-    let huo = path!("huo" / String / String)
+    let huo = warp::get()
+        .and(check_params())
         .and(warp::header("user-agent"))
-        .map(|name, place, agent: String| {
-            format!("huo: Welcome {} from {} using {}!\n", name, place, agent)
-        });
+        .map(|c: (String, String), agent: String| {
+            format!("huo: Welcome {} from {} using {}!\n", c.0, c.1, agent)
+        })
+        .recover(handler);
 
-    let routes = warp::get().and(root.or(help).or(huo));
+    let routes = warp::get().and(root.or(help)).or(huo);
     warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
+}
+
+fn check_params() -> impl Filter<Extract = ((String, String),), Error = Rejection> + Copy {
+    path!("huo" / String / String).and_then(|name: String, place| async move {
+        if name.as_str() == "foo" || name.as_str() == "bar" {
+            return Err(reject::custom(IllegalInput));
+        }
+
+        Ok((name, place))
+    })
+}
+
+async fn handler(err: Rejection) -> Result<impl Reply, std::convert::Infallible> {
+    if err.is_not_found() {
+        Ok(reply::with_status(
+            "Invalid request !",
+            StatusCode::NOT_FOUND,
+        ))
+    } else if let Some(_e) = err.find::<IllegalInput>() {
+        Ok(reply::with_status("FORBIDDEN", StatusCode::FORBIDDEN))
+    } else {
+        println!("[DEBUG] Unhandled rejection: {:?}", err);
+        Ok(reply::with_status(
+            "INTERNAL_SERVER_ERROR",
+            StatusCode::INTERNAL_SERVER_ERROR,
+        ))
+    }
 }
