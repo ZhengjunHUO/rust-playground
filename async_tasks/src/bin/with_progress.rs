9f3d8952fa085293572ad2519b5eb6b9d0f6114f
ZhengjunHUO <firelouiszj@hotmail.com>
2024-04-04 18:36:23 +0200

    refacto async tasks
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- async_tasks/src/bin/with_progress.rs
+++ async_tasks/src/bin/with_progress.rs
@@ -4,25 +4,23 @@ use rand::{Rng, SeedableRng};
 use std::collections::VecDeque;
 use std::env;
 use std::fs::read_to_string;
-use std::future::Future;
 use std::sync::{Arc, Mutex};
 use std::{thread, time};
 use tokio::sync::mpsc::{channel, Sender};
 
-async fn handle(ctx: Context, tx: Sender<Payload>) {
-    let client_id = ctx.client_id;
+async fn handle(idx: u8, client_id: String, table_list: TableList, tx: Sender<String>) -> String {
     println!("ckh-{}: Worker start !", client_id);
     loop {
         let table;
 
         {
-            let mut garde = ctx.table_list.lock().unwrap();
+            let mut garde = table_list.lock().unwrap();
             table = garde.pop_front();
         }
 
         let seed = [
             1, 0, 0, 0, 23, 0, 0, 0, 200, 1, 0, 0, 210, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, ctx.idx,
+            0, 0, 0, 0, idx,
         ];
         let mut rng = StdRng::from_seed(seed);
 
@@ -30,12 +28,10 @@ async fn handle(ctx: Context, tx: Sender<Payload>) {
             Some(table_name) => {
                 let secs = rng.gen_range(3..=8);
                 if let Err(e) = tx
-                    .send(Payload {
-                        message: format!(
-                            "[ckh-{}] Dealing with the table {}, estimate done in {} secs",
-                            client_id, table_name, secs
-                        ),
-                    })
+                    .send(format!(
+                        "[ckh-{}] Dealing with the table {}, estimate done in {} secs",
+                        client_id, table_name, secs
+                    ))
                     .await
                 {
                     println!(
@@ -51,9 +47,10 @@ async fn handle(ctx: Context, tx: Sender<Payload>) {
                 */
                 thread::sleep(time::Duration::from_secs(secs));
                 if let Err(e) = tx
-                    .send(Payload {
-                        message: format!("[ckh-{}] Done with the table {}", client_id, table_name),
-                    })
+                    .send(format!(
+                        "[ckh-{}] Done with the table {}",
+                        client_id, table_name
+                    ))
                     .await
                 {
                     println!(
@@ -65,12 +62,10 @@ async fn handle(ctx: Context, tx: Sender<Payload>) {
             }
             None => {
                 if let Err(e) = tx
-                    .send(Payload {
-                        message: format!(
-                            "[ckh-{}] Todo list is empty, mission complete",
-                            client_id
-                        ),
-                    })
+                    .send(format!(
+                        "[ckh-{}] Todo list is empty, mission complete",
+                        client_id
+                    ))
                     .await
                 {
                     println!(
@@ -83,30 +78,13 @@ async fn handle(ctx: Context, tx: Sender<Payload>) {
             }
         }
     }
+    "Done".to_string()
 }
 
 type TableList = Arc<Mutex<VecDeque<String>>>;
 
-struct Context {
-    idx: u8,
-    client_id: String,
-    table_list: TableList,
-}
-
-struct Payload {
-    message: String,
-}
-
 #[tokio::main]
 async fn main() {
-    doit(handle).await
-}
-
-async fn doit<F, Fut>(handle_func: F)
-where
-    F: Fn(Context, Sender<Payload>) -> Fut,
-    Fut: Future<Output = ()> + Send + 'static,
-{
     let path = env::args()
         .nth(1)
         .expect("Expect a path to file containing list of tables to be dealt with.");
@@ -135,12 +113,12 @@ where
     let ind = ProgressBar::new(num_job);
     for (i, ep) in eps.into_iter().enumerate() {
         let list = tables.clone();
-        let context = Context {
-            idx: i as u8,
-            client_id: ep,
-            table_list: list,
-        };
-        tasks.push(tokio::spawn(handle_func(context, senders.pop().unwrap())));
+        tasks.push(tokio::spawn(handle(
+            i as u8,
+            ep,
+            list,
+            senders.pop().unwrap(),
+        )));
     }
 
     /*
@@ -151,9 +129,9 @@ where
     */
 
     println!("[main] Receiving message !");
-    while let Some(payload) = rx.recv().await {
-        ind.println(payload.message.to_string());
-        if payload.message.contains("Done") {
+    while let Some(msg) = rx.recv().await {
+        ind.println(msg.to_string());
+        if msg.contains("Done") {
             ind.inc(1);
         }
     }
