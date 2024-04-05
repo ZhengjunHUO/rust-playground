3876a5ace27d3df2551d6f2d35c74914c334489e
ZhengjunHUO <firelouiszj@hotmail.com>
2024-04-05 11:41:11 +0200

    refacto async tasks #3
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- async_tasks/src/main.rs
+++ async_tasks/src/main.rs
@@ -1,4 +1,5 @@
 use indicatif::ProgressBar;
+use std::cmp::min;
 use std::collections::VecDeque;
 use std::env;
 use std::fs::read_to_string;
@@ -7,96 +8,141 @@ use std::sync::{Arc, Mutex};
 use std::{thread, time};
 use tokio::sync::mpsc::{channel, Sender};
 
+type SharedBacklog = Arc<Mutex<VecDeque<String>>>;
+
+fn prepare_shared_backlog() -> (SharedBacklog, usize) {
+    let path = env::args()
+        .nth(1)
+        .expect("Expect a path to file containing list of tables to be dealt with.");
+
+    let mut table_list = VecDeque::new();
+    for line in read_to_string(path).unwrap().lines() {
+        table_list.push_back(line.to_string())
+    }
+    let num_job = table_list.len();
+
+    (Arc::new(Mutex::new(table_list)), num_job)
+}
+
+struct Context {
+    client_id: String,
+    table_list: SharedBacklog,
+}
+
+struct Payload {
+    message: String,
+    done: bool,
+}
+
+impl Payload {
+    fn new(message: String, done: bool) -> Self {
+        Payload { message, done }
+    }
+}
+
+macro_rules! send_message {
+    ($tx:ident, $client_id: ident, $payload: ident) => {
+        if let Err(e) = $tx.send($payload).await {
+            println!(
+                "[WARN] ckh-{}: Send message with error returned: {}",
+                $client_id, e
+            );
+        };
+    };
+}
+
+macro_rules! do_async_tasks {
+    ($eps:ident, $num_job:ident, $tables:ident, $handle_func:ident) => {
+        // Prepare mpsc channels
+        let (tx, mut rx) = channel(min($num_job, 10));
+        let mut senders = Vec::with_capacity($eps.len());
+        (0..$eps.len() - 1).for_each(|_| senders.push(tx.clone()));
+        senders.push(tx);
+
+        // Optional: init progress bar
+        let ind = ProgressBar::new($num_job as u64);
+
+        // Dispatch jobs to workers
+        let mut tasks = Vec::with_capacity($eps.len());
+        for ep in $eps.into_iter() {
+            let context = Context {
+                client_id: ep,
+                table_list: $tables.clone(),
+            };
+            tasks.push(tokio::spawn($handle_func(context, senders.pop().unwrap())));
+        }
+
+        println!("[main] Receiving message !");
+        while let Some(payload) = rx.recv().await {
+            // Optional: update progress bar
+            ind.println(&payload.message);
+            if payload.done {
+                ind.inc(1);
+            }
+        }
+
+        // Optional: quit progress bar
+        ind.finish_with_message("Complete");
+
+        println!("[main] All done!");
+    };
+}
+
+fn prepare_workers() -> Vec<String> {
+    ["0-0", "0-1", "1-0", "1-1"]
+        .iter()
+        .map(|&s| s.into())
+        .collect()
+}
+
 async fn handle(ctx: Context, tx: Sender<Payload>) {
     let client_id = ctx.client_id;
     println!("ckh-{}: Worker start !", client_id);
+
     loop {
         let table;
-
         {
             let mut garde = ctx.table_list.lock().unwrap();
             table = garde.pop_front();
         }
 
+        let secs = 3;
+
         match table {
             Some(table_name) => {
-                let secs = 3;
-                if let Err(e) = tx
-                    .send(Payload {
-                        message: format!(
-                            "[ckh-{}] Dealing with the table {}, estimate done in {} secs",
-                            client_id, table_name, secs
-                        ),
-                    })
-                    .await
-                {
-                    println!(
-                        "[WARN] ckh-{}: Send message with error returned: {}",
-                        client_id, e
-                    );
-                };
-                /*
-                println!(
-                    "[ckh-{}] Dealing with the table {}, estimate done in {} secs",
-                    client_id, table_name, secs
+                // Notify start task
+                let payload = Payload::new(
+                    format!(
+                        "[ckh-{}] Dealing with the table {}, estimate done in {} secs",
+                        client_id, table_name, secs
+                    ),
+                    false,
                 );
-                */
+                send_message!(tx, client_id, payload);
+
+                // Do sth
                 thread::sleep(time::Duration::from_secs(secs));
-                if let Err(e) = tx
-                    .send(Payload {
-                        message: format!("[ckh-{}] Done with the table {}", client_id, table_name),
-                    })
-                    .await
-                {
-                    println!(
-                        "[WARN] ckh-{}: Send message with error returned: {}",
-                        client_id, e
-                    );
-                };
-                //println!("[ckh-{}] Done with the table {}", client_id, table_name);
+
+                // Notify end task
+                let payload = Payload::new(
+                    format!("[ckh-{}] Done with the table {}", client_id, table_name),
+                    true,
+                );
+                send_message!(tx, client_id, payload);
             }
             None => {
-                if let Err(e) = tx
-                    .send(Payload {
-                        message: format!(
-                            "[ckh-{}] Todo list is empty, mission complete",
-                            client_id
-                        ),
-                    })
-                    .await
-                {
-                    println!(
-                        "[WARN] ckh-{}: Send message with error returned: {}",
-                        client_id, e
-                    );
-                };
-                //println!("[ckh-{}] Todo list is empty, mission complete", client_id);
+                // Notify quit
+                let payload = Payload::new(
+                    format!("[ckh-{}] Todo list is empty, mission complete", client_id),
+                    false,
+                );
+                send_message!(tx, client_id, payload);
+
                 break;
             }
         }
     }
 }
-
-type TableList = Arc<Mutex<VecDeque<String>>>;
-
-struct Context {
-    client_id: String,
-    table_list: TableList,
-}
-
-struct Payload {
-    message: String,
-}
-
-/*
-struct Async_Sched {
-
-}
-
-impl Async_Sched {
-
-}
-*/
 
 #[tokio::main]
 async fn main() {
@@ -108,57 +154,8 @@ where
     F: Fn(Context, Sender<Payload>) -> Fut,
     Fut: Future<Output = ()> + Send + 'static,
 {
-    let path = env::args()
-        .nth(1)
-        .expect("Expect a path to file containing list of tables to be dealt with.");
+    let (tables, num_job) = prepare_shared_backlog();
+    let eps = prepare_workers();
 
-    let mut table_list = VecDeque::new();
-    for line in read_to_string(path).unwrap().lines() {
-        table_list.push_back(line.to_string())
-    }
-    let num_job = table_list.len() as u64;
-
-    let tables = Arc::new(Mutex::new(table_list));
-
-    let eps: Vec<String> = ["0-0", "0-1", "1-0", "1-1"]
-        .iter()
-        .map(|&s| s.into())
-        .collect();
-    let mut tasks = Vec::with_capacity(eps.len());
-
-    let (tx, mut rx) = channel(8);
-    let mut senders = Vec::with_capacity(eps.len());
-    for _ in 0..eps.len() - 1 {
-        senders.push(tx.clone());
-    }
-    senders.push(tx);
-
-    let ind = ProgressBar::new(num_job);
-    for ep in eps.into_iter() {
-        let list = tables.clone();
-        let context = Context {
-            client_id: ep,
-            table_list: list,
-        };
-        tasks.push(tokio::spawn(handle_func(context, senders.pop().unwrap())));
-    }
-
-    /*
-    let mut rslt = Vec::with_capacity(tasks.len());
-    for task in tasks {
-        rslt.push(task.await.unwrap());
-    }
-    */
-
-    println!("[main] Receiving message !");
-    while let Some(payload) = rx.recv().await {
-        ind.println(&payload.message);
-        if payload.message.contains("Done") {
-            ind.inc(1);
-        }
-    }
-    ind.finish_with_message("Complete");
-
-    //println!("{:?}", rslt);
-    println!("[main] All done!");
+    do_async_tasks!(eps, num_job, tables, handle_func);
 }
