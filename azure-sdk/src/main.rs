f22512ce13f2906dda523fa665875c1d41facefa
ZhengjunHUO <firelouiszj@hotmail.com>
2024-11-25 15:37:28 +0100

    test azure container client with list action
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- azure-sdk/src/main.rs
+++ azure-sdk/src/main.rs
@@ -1,33 +1,47 @@
+use anyhow::{bail, Result};
 use azure_storage::prelude::*;
 use azure_storage_blobs::prelude::*;
 use futures::stream::StreamExt;
 
+trait Crud {
+    async fn list(&self) -> Result<Vec<String>>;
+}
+
+impl Crud for ContainerClient {
+    async fn list(&self) -> Result<Vec<String>> {
+        let list = self.list_blobs();
+
+        let mut rslt = Vec::new();
+
+        let mut stream = list.into_stream();
+        while let Some(resp) = stream.next().await {
+            println!("[DEBUG] Found something");
+            match resp {
+                Ok(elem) => {
+                    let blobs: Vec<_> = elem.blobs.blobs().collect();
+                    blobs.into_iter().for_each(|blob| {
+                        rslt.push(blob.name.clone());
+                        println!("[DEBUG] {:?}", blob)
+                    });
+                }
+                _ => bail!("Sth wrong happened"),
+            }
+        }
+        Ok(rslt)
+    }
+}
+
 #[tokio::main]
-async fn main() -> azure_core::Result<()> {
+async fn main() -> Result<()> {
     let account = std::env::var("AZURE_ACCOUNT_NAME").expect("AZURE_ACCOUNT_NAME not set");
     let access_key = std::env::var("AZURE_ACCESS_KEY").expect("AZURE_ACCESS_KEY not set");
     let container = String::from("rustacean");
-    let blob_name = String::from("foo");
+    //let blob_name = String::from("bar/readme");
 
     let storage_credentials = StorageCredentials::access_key(account.clone(), access_key);
-    let blob_client =
-        ClientBuilder::new(account, storage_credentials).blob_client(&container, blob_name);
+    let container_client =
+        ClientBuilder::new(account, storage_credentials).container_client(&container);
+    println!("Result: {:?}", container_client.list().await?);
 
-    blob_client
-        .put_block_blob("Rust rocks!")
-        .content_type("text/plain")
-        .await?;
-
-    let mut result: Vec<u8> = vec![];
-    let mut stream = blob_client.get().into_stream();
-    while let Some(value) = stream.next().await {
-        let mut body = value?.data;
-        while let Some(value) = body.next().await {
-            let value = value?;
-            result.extend(&value);
-        }
-    }
-
-    println!("result: {}", String::from_utf8_lossy(&result));
     Ok(())
 }
