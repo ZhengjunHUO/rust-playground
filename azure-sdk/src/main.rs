e0b1732446f67ec58d66f9894d020dbbbedf1dfe
ZhengjunHUO <firelouiszj@hotmail.com>
2024-11-25 17:13:58 +0100

    test azure container client with put action
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- azure-sdk/src/main.rs
+++ azure-sdk/src/main.rs
@@ -5,6 +5,7 @@ use futures::stream::StreamExt;
 
 trait Crud {
     async fn list(&self) -> Result<Vec<String>>;
+    async fn put_obj(&self, path: String, content: &[u8]) -> Result<()>;
 }
 
 impl Crud for ContainerClient {
@@ -29,6 +30,15 @@ impl Crud for ContainerClient {
         }
         Ok(rslt)
     }
+
+    async fn put_obj(&self, path: String, content: &[u8]) -> Result<()> {
+        let resp = self
+            .blob_client(path)
+            .put_block_blob(content.to_vec())
+            .await?;
+        println!("[DEBUG] Got resp: {:?}", resp);
+        Ok(())
+    }
 }
 
 #[tokio::main]
@@ -36,11 +46,15 @@ async fn main() -> Result<()> {
     let account = std::env::var("AZURE_ACCOUNT_NAME").expect("AZURE_ACCOUNT_NAME not set");
     let access_key = std::env::var("AZURE_ACCESS_KEY").expect("AZURE_ACCESS_KEY not set");
     let container = String::from("rustacean");
-    //let blob_name = String::from("bar/readme");
 
     let storage_credentials = StorageCredentials::access_key(account.clone(), access_key);
     let container_client =
         ClientBuilder::new(account, storage_credentials).container_client(&container);
+
+    let content = String::from("RTFM");
+    container_client
+        .put_obj(String::from("bar/baz/readme"), content.as_bytes())
+        .await?;
     println!("Result: {:?}", container_client.list().await?);
 
     Ok(())
