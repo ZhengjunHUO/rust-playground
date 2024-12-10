77c53f599c6da055354c721757f1ff7c5eaa3793
ZhengjunHUO <firelouiszj@hotmail.com>
2024-12-10 16:04:57 +0100

    explore trait object
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- generics/src/bin/aggr_struct.rs
+++ generics/src/bin/aggr_struct.rs
@@ -6,19 +6,23 @@ trait Bar {
     fn do_bar(&self);
 }
 
-struct SomeFoo {}
+struct SomeFoo {
+    num: u8,
+}
 struct SomeBar {}
-struct AnotherFoo {}
+struct AnotherFoo {
+    num: u8,
+}
 
 impl Foo for SomeFoo {
     fn do_foo(&self) {
-        println!("SomeFoo is speaking");
+        println!("SomeFoo {} is speaking", self.num);
     }
 }
 
 impl Foo for AnotherFoo {
     fn do_foo(&self) {
-        println!("AnotherFoo is speaking");
+        println!("AnotherFoo {} is speaking", self.num);
     }
 }
 
@@ -30,17 +34,26 @@ impl Bar for SomeBar {
 
 trait AggrTrait {
     fn do_something(&self);
+    fn get_bar_ref(&self) -> &dyn Bar;
 }
 
 struct Aggr<T1, T2> {
-    mem1: T1,
-    mem2: T2,
+    mem1: Vec<T1>,
+    mem2: Option<T2>,
 }
 
 impl<T1: Foo, T2: Bar> AggrTrait for Aggr<T1, T2> {
     fn do_something(&self) {
-        self.mem1.do_foo();
-        self.mem2.do_bar();
+        self.mem1.iter().for_each(|elem| elem.do_foo());
+
+        match &self.mem2 {
+            Some(obj) => obj.do_bar(),
+            None => (),
+        }
+    }
+
+    fn get_bar_ref(&self) -> &dyn Bar {
+        self.mem2.as_ref().unwrap()
     }
 }
 
@@ -48,17 +61,31 @@ fn handle(instance: &dyn AggrTrait) {
     instance.do_something();
 }
 
+fn new_foo(num: u8) -> SomeFoo {
+    SomeFoo { num }
+}
+
+fn new_another(num: u8) -> AnotherFoo {
+    AnotherFoo { num }
+}
+
+fn new_bar() -> SomeBar {
+    SomeBar {}
+}
+
 fn main() {
     let instance: Box<dyn AggrTrait> = match std::env::var("USE_ANOTHER_FOO") {
         Ok(_) => Box::new(Aggr {
-            mem1: AnotherFoo {},
-            mem2: SomeBar {},
+            mem1: vec![new_another(0), new_another(1), new_another(2)],
+            mem2: Some(new_bar()),
         }),
         Err(_) => Box::new(Aggr {
-            mem1: SomeFoo {},
-            mem2: SomeBar {},
+            mem1: vec![new_foo(0), new_foo(1), new_foo(2)],
+            mem2: Some(new_bar()),
         }),
     };
+
+    instance.get_bar_ref().do_bar();
 
     handle(&*instance);
 }
