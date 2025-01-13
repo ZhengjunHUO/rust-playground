dc3cfdfc1023d785c1bc81d6cedb4effa4b5108d
ZhengjunHUO <firelouiszj@hotmail.com>
2025-01-13 20:35:55 +0100

    coroutine example #2
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- async_rust_oreilly/src/bin/ch5_coroutine_file_writing.rs
+++ async_rust_oreilly/src/bin/ch5_coroutine_file_writing.rs
@@ -1,27 +1,54 @@
 #![feature(coroutines)]
 #![feature(coroutine_trait)]
 use rand::Rng;
-use std::fs::OpenOptions;
+use std::fs::{File, OpenOptions};
 use std::io::{self, Write};
 use std::time::Instant;
 
-fn add_num_to_file(n: i32) -> io::Result<()> {
-    let mut file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open("foo.txt")?;
-    writeln!(file, "{}", n)?;
-    Ok(())
+use std::ops::{Coroutine, CoroutineState};
+use std::pin::Pin;
+
+struct FdWrapper {
+    fd: File,
 }
+
+impl FdWrapper {
+    fn new(path: &str) -> io::Result<Self> {
+        let file = OpenOptions::new().create(true).append(true).open(path)?;
+        Ok(Self { fd: file })
+    }
+}
+
+impl Coroutine<i32> for FdWrapper {
+    type Yield = ();
+    type Return = ();
+
+    fn resume(mut self: Pin<&mut Self>, arg: i32) -> CoroutineState<Self::Yield, Self::Return> {
+        writeln!(self.fd, "{}", arg).unwrap();
+        CoroutineState::Yielded(())
+    }
+}
+
+// fn add_num_to_file(n: i32) -> io::Result<()> {
+//     let mut file = OpenOptions::new()
+//         .create(true)
+//         .append(true)
+//         .open("foo.txt")?;
+//     writeln!(file, "{}", n)?;
+//     Ok(())
+// }
 
 fn main() -> io::Result<()> {
     let mut rng = rand::thread_rng();
     let nums = (0..100000).map(|_| rng.gen()).collect::<Vec<i32>>();
     let begin = Instant::now();
+
+    let mut coroutine = FdWrapper::new("foo.txt")?;
     for num in nums {
-        if let Err(e) = add_num_to_file(num) {
-            eprintln!("Error occurred writing to file: {e}");
-        }
+        Pin::new(&mut coroutine).resume(num);
+        // if let Err(e) = add_num_to_file(num) {
+        //     eprintln!("Error occurred writing to file: {e}");
+        // }
     }
     let elapsed = begin.elapsed();
     println!("Task took {:?}", elapsed);
