62ae11c79aa85dc64dcc63bcac8c4b56cf8a271d
ZhengjunHUO <firelouiszj@hotmail.com>
2025-01-28 14:16:19 +0100

    basic actors comparaison
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- async_rust_oreilly/src/bin/ch8_actor.rs
+++ async_rust_oreilly/src/bin/ch8_actor.rs
@@ -1,4 +1,6 @@
-use tokio::sync::{mpsc::Receiver, oneshot};
+use std::sync::Arc;
+
+use tokio::sync::{mpsc::Receiver, oneshot, Mutex};
 
 struct SimplePayload {
     value: usize,
@@ -10,10 +12,12 @@ async fn simple_actor(mut rx: Receiver<SimplePayload>) {
     let mut counter = 0;
     while let Some(msg) = rx.recv().await {
         counter += msg.value;
+        /*
         println!(
             "[Actor] Recv value: {}; Total count: {}",
             msg.value, counter
         );
+        */
         if let Err(err) = msg.reply.send(format!("[Actor] Recv value: {}", msg.value)) {
             eprint!(
                 "[Actor] Error occurred sending message back to main: {}",
@@ -23,8 +27,72 @@ async fn simple_actor(mut rx: Receiver<SimplePayload>) {
     }
 }
 
-#[tokio::main]
-async fn main() {
+async fn simple_actor_using_mutex(counter: Arc<Mutex<usize>>, value: usize) {
+    let mut guard = counter.lock().await;
+    *guard += value;
+}
+
+async fn test_with_simple_actor_using_mutex(round: usize) {
+    println!("[test_with_simple_actor_using_mutex] Running ...");
+
+    let counter = Arc::new(Mutex::new(0));
+    let mut handlers = Vec::new();
+    let now = tokio::time::Instant::now();
+
+    for i in 0..round {
+        let counter_clone = counter.clone();
+        handlers.push(tokio::spawn(async move {
+            simple_actor_using_mutex(counter_clone, i).await
+        }));
+    }
+
+    for h in handlers {
+        h.await.unwrap();
+    }
+
+    println!(
+        "[test_with_simple_actor_using_mutex] Done <{:?}>",
+        now.elapsed()
+    );
+}
+
+async fn test_with_simple_actor(round: usize) {
+    println!("[test_with_simple_actor] Running ...");
+
+    let (tx, rx) = tokio::sync::mpsc::channel::<SimplePayload>(round);
+    tokio::spawn(simple_actor(rx));
+
+    let mut handlers = Vec::new();
+    let now = tokio::time::Instant::now();
+
+    for i in 0..round {
+        let tx_clone = tx.clone();
+
+        handlers.push(tokio::spawn(async move {
+            let (one_tx, one_rx) = oneshot::channel::<String>();
+            tx_clone
+                .send(SimplePayload {
+                    value: i,
+                    reply: one_tx,
+                })
+                .await
+                .unwrap();
+
+            match one_rx.await {
+                Ok(_) => {}
+                Err(_) => println!("[Main] Can't recv ack from actor"),
+            }
+        }));
+    }
+
+    for h in handlers {
+        h.await.unwrap();
+    }
+
+    println!("[test_with_simple_actor] Done <{:?}>", now.elapsed());
+}
+/*
+async fn test_with_simple_actor() {
     let (tx, rx) = tokio::sync::mpsc::channel::<SimplePayload>(20);
 
     tokio::spawn(simple_actor(rx));
@@ -41,4 +109,12 @@ async fn main() {
             Err(_) => println!("[Main] Can't recv ack from actor"),
         }
     }
+}
+*/
+
+#[tokio::main]
+async fn main() {
+    let round = 10000000;
+    test_with_simple_actor_using_mutex(round).await;
+    test_with_simple_actor(round).await;
 }
