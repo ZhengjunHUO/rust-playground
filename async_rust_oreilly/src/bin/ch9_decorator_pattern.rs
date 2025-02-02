f7fa464f37b652163042c95d6e5165f0e945a414
ZhengjunHUO <firelouiszj@hotmail.com>
2025-02-02 20:25:56 +0100

    decorator pattern #2
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- async_rust_oreilly/src/bin/ch9_decorator_pattern.rs
+++ async_rust_oreilly/src/bin/ch9_decorator_pattern.rs
@@ -1,3 +1,5 @@
+use std::{future::Future, pin::Pin};
+
 trait Echo {
     fn echo(&self) -> String;
 }
@@ -25,9 +27,67 @@ where
     }
 }
 
-fn main() {
+trait Logger {
+    fn log(&self);
+}
+
+/*
+struct LoggerWrapper<F: Future + Logger> {
+    inner: F,
+}
+
+impl<F: Future + Logger> Future for LoggerWrapper<F> {
+    type Output = F::Output;
+
+    fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
+        let inner = unsafe { self.map_unchecked_mut(|s| &mut s.inner) };
+        inner.log();
+        inner.poll(cx)
+    }
+}
+ */
+
+struct LoggerWrapper<F: Future + Logger> {
+    inner: Pin<Box<F>>,
+}
+
+impl<F: Future + Logger> Future for LoggerWrapper<F> {
+    type Output = F::Output;
+
+    fn poll(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        let inner = self.get_mut().inner.as_mut();
+        inner.log();
+        inner.poll(cx)
+    }
+}
+
+impl<F: Future> Logger for F {
+    fn log(&self) {
+        println!("Future get polled.");
+    }
+}
+
+async fn demo_future() -> String {
+    "Exec future done.".to_string()
+}
+
+#[tokio::main]
+async fn main() {
+    #[cfg(feature = "decorator_test")]
+    let e: EchoWrapper<Hello> = EchoWrapper { echoer: Hello };
+    #[cfg(not(feature = "decorator_test"))]
     let e = Hello;
-    let w = EchoWrapper { echoer: Hello };
     println!("{}", e.echo());
-    println!("{}", w.echo());
+
+    /* 输出为：
+    Future get polled.
+    Result: Exec future done.
+     */
+    let wrapped_future = LoggerWrapper {
+        inner: Box::pin(demo_future()),
+    };
+    println!("Result: {}", wrapped_future.await);
 }
