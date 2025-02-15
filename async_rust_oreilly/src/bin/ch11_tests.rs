d4b20034fbe0f8442115a574ceb79858fe6b8e65
ZhengjunHUO <firelouiszj@hotmail.com>
2025-02-15 22:44:15 +0100

    test async func with mockall
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- async_rust_oreilly/src/bin/ch11_tests.rs
+++ async_rust_oreilly/src/bin/ch11_tests.rs
@@ -1,15 +1,35 @@
-pub trait AsyncProcess<X, Y, Z> {
+use std::future::Future;
+
+pub trait SyncProcess<X, Y, Z> {
     fn spawn(&self, input: X) -> Result<Y, String>;
     fn retrieve(&self, key: Y) -> Result<Z, String>;
 }
 
-fn handle<T>(handle: T, input: i32) -> Result<i32, String>
+pub fn handle_sync<T>(handle: T, input: i32) -> Result<i32, String>
 where
-    T: AsyncProcess<i32, String, i32>,
+    T: SyncProcess<i32, String, i32>,
 {
     let key = handle.spawn(input)?;
     println!("Key received, retrieve answer");
     let rslt = handle.retrieve(key)?;
+    if rslt > 10 {
+        return Err(String::from("Too big."));
+    }
+    if rslt == 8 {
+        return Ok(rslt * 10);
+    }
+    Ok(rslt * 3)
+}
+
+pub trait AsyncProcess<Y, Z> {
+    fn retrieve(&self, key: Y) -> impl Future<Output = Result<Z, String>>;
+}
+
+pub async fn handle_async<T>(handle: T, input: i32) -> Result<i32, String>
+where
+    T: AsyncProcess<i32, i32>,
+{
+    let rslt = handle.retrieve(input).await?;
     if rslt > 10 {
         return Err(String::from("Too big."));
     }
@@ -27,14 +47,21 @@ mod tests {
 
     mock! {
         S3Handler {}
-        impl AsyncProcess<i32, String, i32> for S3Handler {
+        impl SyncProcess<i32, String, i32> for S3Handler {
             fn spawn(&self, input: i32) -> Result<String, String>;
             fn retrieve(&self, key: String) -> Result<i32, String>;
         }
     }
 
+    mock! {
+        DBHandler {}
+        impl AsyncProcess<i32, i32> for DBHandler {
+            fn retrieve(&self, key: i32) -> impl Future<Output = Result<i32, String>>;
+        }
+    }
+
     #[test]
-    fn test_handle_ok() {
+    fn test_handle_sync_ok() {
         let mut h = MockS3Handler::new();
         h.expect_spawn()
             .with(eq(9))
@@ -43,12 +70,12 @@ mod tests {
             .with(eq(String::from("test_key")))
             .returning(|_| Ok(8));
 
-        let rslt = handle(h, 9);
+        let rslt = handle_sync(h, 9);
         assert_eq!(rslt, Ok(80));
     }
 
     #[test]
-    fn test_handle_ko() {
+    fn test_handle_sync_ko() {
         // Arrange
         let mut h = MockS3Handler::new();
         h.expect_spawn()
@@ -59,9 +86,38 @@ mod tests {
             .returning(|_| Ok(11));
 
         // Act
-        let rslt = handle(h, 4);
+        let rslt = handle_sync(h, 4);
 
         // Assert
+        assert_eq!(rslt, Err(String::from("Too big.")));
+    }
+
+    #[test]
+    fn test_handle_async_ok() {
+        let mut h = MockDBHandler::new();
+        h.expect_retrieve()
+            .with(eq(2))
+            .returning(|_| Box::pin(async { Ok(9) }));
+        let runtime = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+        let rslt = runtime.block_on(handle_async(h, 2));
+        assert_eq!(rslt, Ok(27));
+    }
+
+    #[test]
+    fn test_handle_async_ko() {
+        let mut h = MockDBHandler::new();
+        h.expect_retrieve()
+            .with(eq(5))
+            .returning(|_| Box::pin(async { Ok(20) }));
+
+        let runtime = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+        let rslt = runtime.block_on(handle_async(h, 5));
         assert_eq!(rslt, Err(String::from("Too big.")));
     }
 }
