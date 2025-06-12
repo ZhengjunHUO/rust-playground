e652ded479a4216a9ee94f699d1b3fe11e5f678b
ZhengjunHUO <firelouiszj@hotmail.com>
2025-06-12 10:31:35 +0200

    test graphql
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

---
+++ oidc/src/bin/graphql.rs
@@ -0,0 +1,105 @@
+use std::{io, sync::Arc};
+
+use actix_cors::Cors;
+use actix_web::{
+    get, middleware, route,
+    web::{self, Data},
+    App, HttpResponse, HttpServer, Responder,
+};
+use juniper::http::{graphiql::graphiql_source, GraphQLRequest};
+use juniper::{
+    EmptySubscription, FieldResult, GraphQLEnum, GraphQLInputObject, GraphQLObject, RootNode,
+};
+
+/// playground UI
+#[get("/graphiql")]
+async fn graphql_playground() -> impl Responder {
+    web::Html::new(graphiql_source("/graphql", None))
+}
+
+/// GraphQL endpoint
+#[route("/graphql", method = "GET", method = "POST")]
+async fn graphql(st: web::Data<Schema>, data: web::Json<GraphQLRequest>) -> impl Responder {
+    let user = data.execute(&st, &()).await;
+    HttpResponse::Ok().json(user)
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    let schema = Arc::new(create_schema());
+
+    log::info!("Listening on 8080. playground URL: http://localhost:8080/graphiql");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Data::from(schema.clone()))
+            .service(graphql)
+            .service(graphql_playground)
+            .wrap(Cors::permissive())
+            .wrap(middleware::Logger::default())
+    })
+    .workers(2)
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+#[derive(GraphQLEnum)]
+enum Episode {
+    NewHope,
+    Empire,
+    Jedi,
+}
+
+#[derive(GraphQLObject)]
+#[graphql(description = "A humanoid creature in the Star Wars universe")]
+struct Human {
+    id: String,
+    name: String,
+    appears_in: Vec<Episode>,
+    home_planet: String,
+}
+
+#[derive(GraphQLInputObject)]
+#[graphql(description = "A humanoid creature in the Star Wars universe")]
+struct NewHuman {
+    name: String,
+    appears_in: Vec<Episode>,
+    home_planet: String,
+}
+
+struct QueryRoot;
+
+#[juniper::graphql_object]
+impl QueryRoot {
+    fn human(_id: String) -> FieldResult<Human> {
+        Ok(Human {
+            id: "1234".to_owned(),
+            name: "Luke".to_owned(),
+            appears_in: vec![Episode::NewHope],
+            home_planet: "Mars".to_owned(),
+        })
+    }
+}
+
+struct MutationRoot;
+
+#[juniper::graphql_object]
+impl MutationRoot {
+    fn create_human(new_human: NewHuman) -> FieldResult<Human> {
+        Ok(Human {
+            id: "1234".to_owned(),
+            name: new_human.name,
+            appears_in: new_human.appears_in,
+            home_planet: new_human.home_planet,
+        })
+    }
+}
+
+type Schema = RootNode<'static, QueryRoot, MutationRoot, EmptySubscription>;
+
+fn create_schema() -> Schema {
+    Schema::new(QueryRoot {}, MutationRoot {}, EmptySubscription::new())
+}
