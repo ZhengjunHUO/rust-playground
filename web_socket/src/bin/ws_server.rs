1039fae0f392c0f858a475ffe29b2e1c5c1eb439
ZhengjunHUO <firelouiszj@hotmail.com>
2025-11-06 13:56:25 +0100

    feat: ws server works on ws/wss
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- web_socket/src/bin/ws_server.rs
+++ web_socket/src/bin/ws_server.rs
@@ -7,31 +7,76 @@ use std::{
     net::SocketAddr,
     sync::{Arc, Mutex},
 };
-use tokio::net::{TcpListener, TcpStream};
-use tokio_tungstenite::{accept_async, tungstenite::Message};
+use tokio::net::TcpListener;
+use tokio_native_tls::{TlsAcceptor, native_tls};
+use tokio_tungstenite::{WebSocketStream, accept_async, tungstenite::Message};
 
 type ConnManager = Arc<Mutex<HashMap<SocketAddr, UnboundedSender<Message>>>>;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let mut tls_acceptor: Option<TlsAcceptor> = None;
+
+    let tls_enabled = env::args()
+        .nth(2)
+        .map(|arg| !arg.is_empty())
+        .unwrap_or(false);
+    if tls_enabled {
+        let cert = std::fs::read("server.crt.pem").expect("Error reading server certificate");
+        let key = std::fs::read("server.key.pem").expect("Error reading server key");
+
+        let id = native_tls::Identity::from_pkcs8(&cert, &key)
+            .expect("Error preparing identity from cert and key");
+        tls_acceptor = Some(TlsAcceptor::from(
+            native_tls::TlsAcceptor::builder(id)
+                .build()
+                .expect("Error building tls acceptor"),
+        ));
+    }
+
     let sock = env::args().nth(1).unwrap_or("127.0.0.1:8888".to_owned());
     let listener = TcpListener::bind(&sock).await?;
-    println!("Server up on {}", sock);
+    println!(
+        "Server up on {}{}",
+        if tls_enabled { "wss://" } else { "ws://" },
+        sock
+    );
 
     let manager = ConnManager::new(Mutex::new(HashMap::new()));
 
     while let Ok((conn, addr)) = listener.accept().await {
-        tokio::spawn(handle_conn(conn, addr, manager.clone()));
+        let tls_accptor_clone = tls_acceptor.clone();
+        let manager_clone = manager.clone();
+        tokio::spawn(async move {
+            match tls_accptor_clone {
+                Some(acceptor) => {
+                    let tls_stream = acceptor
+                        .accept(conn)
+                        .await
+                        .expect("Error establishing a connection over TLS");
+                    let stream = accept_async(tls_stream)
+                        .await
+                        .expect("Error occurred accepting a new websocket");
+                    handle_conn(stream, addr, manager_clone).await;
+                }
+                None => {
+                    let stream = accept_async(conn)
+                        .await
+                        .expect("Error occurred accepting a new websocket");
+                    handle_conn(stream, addr, manager_clone).await;
+                }
+            };
+        });
     }
     Ok(())
 }
 
-async fn handle_conn(conn: TcpStream, addr: SocketAddr, manager: ConnManager) {
-    let stream = accept_async(conn)
-        .await
-        .expect("Error occurred accepting a new websocket");
+async fn handle_conn<S>(conn: WebSocketStream<S>, addr: SocketAddr, manager: ConnManager)
+where
+    S: tokio::io::AsyncRead + tokio::io::AsyncWrite + Unpin,
+{
     println!("Connection established with {} !", addr);
-    let (sink, stream) = stream.split();
+    let (sink, stream) = conn.split();
 
     let (tx, rx) = mpsc::unbounded::<Message>();
     manager.lock().unwrap().insert(addr, tx);
