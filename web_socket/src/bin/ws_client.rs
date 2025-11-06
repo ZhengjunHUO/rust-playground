6b2b3c53e7593bae8ae827b44923e3ab4b494f53
ZhengjunHUO <firelouiszj@hotmail.com>
2025-11-06 11:16:42 +0100

    feat: add ws client
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

---
+++ web_socket/src/bin/ws_client.rs
@@ -0,0 +1,68 @@
+use futures_channel::mpsc::{UnboundedSender, unbounded};
+use futures_util::{pin_mut, stream::StreamExt};
+use std::env;
+use tokio::io::{AsyncBufReadExt, BufReader, stdin};
+use tokio_tungstenite::{connect_async, tungstenite::Message};
+
+#[tokio::main]
+async fn main() {
+    let sock = env::args()
+        .nth(1)
+        .unwrap_or("ws://127.0.0.1:8888".to_owned());
+    let (wsstream, _) = connect_async(&sock)
+        .await
+        .expect("Error connecting to ws server");
+    println!("Connected to {}.", sock);
+    let (sink, mut stream) = wsstream.split();
+
+    let (tx, rx) = unbounded::<Message>();
+    tokio::spawn(client_input(tx));
+
+    let recv_and_write = rx.map(Ok).forward(sink);
+    let read_and_print = tokio::spawn(async move {
+        while let Some(msg) = stream.next().await {
+            match msg {
+                Ok(Message::Text(text)) => println!("← Received: {}", text),
+                Ok(Message::Close(_)) => {
+                    println!("Connection closed by server");
+                    break;
+                }
+                Err(e) => {
+                    eprintln!("Error: {}", e);
+                    break;
+                }
+                _ => {}
+            }
+        }
+    });
+
+    pin_mut!(recv_and_write, read_and_print);
+    tokio::select! {
+        _ = recv_and_write => {},
+        _ = read_and_print => {},
+    }
+}
+
+async fn client_input(tx: UnboundedSender<Message>) {
+    let input = stdin();
+    let mut reader = BufReader::new(input);
+    let mut line = String::new();
+
+    loop {
+        line.clear();
+        match reader.read_line(&mut line).await {
+            Ok(0) => break,
+            Ok(_) => {
+                let trimmed = line.trim();
+                if !trimmed.is_empty() {
+                    tx.unbounded_send(Message::Text(trimmed.to_string().into()))
+                        .unwrap();
+                };
+            }
+            Err(e) => {
+                eprintln!("Error reading from stdin: {}", e);
+                break;
+            }
+        }
+    }
+}
